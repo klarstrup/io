8af7e97acba6acda62c70e97ec4b03d8b9d96a22
klarstrup <io@klarstrup.dk>
2023-04-08 02:26:50 +0200

    appDir

---
+++ app/page.tsx
@@ -0,0 +1,41 @@
+import { getIoPercentileForClimbalongCompetition } from "../climbalong";
+import dbConnect from "../dbConnect";
+import { dbFetch } from "../fetch";
+import { Fitocracy } from "../fitocracy";
+
+export default async function Home() {
+  const ioPercentiles = await getData();
+  return <pre>{JSON.stringify(ioPercentiles, null, 2)}</pre>;
+}
+
+const getData = async () => {
+  await dbConnect();
+
+  const activities = await dbFetch<Fitocracy.UserActivity[]>(
+    "https://www.fitocracy.com/get_user_activities/528455/",
+    { headers: { cookie: "sessionid=blahblahblah;" } }
+  );
+  const activityHistories = await Promise.all(
+    activities.map((activity) =>
+      dbFetch(
+        `https://www.fitocracy.com/_get_activity_history_json/?activity-id=${activity.id}`,
+        { headers: { cookie: "sessionid=blahblahblah;" } }
+      )
+    )
+  );
+
+  return Promise.all([
+    getIoPercentileForClimbalongCompetition(13, 844, true),
+    getIoPercentileForClimbalongCompetition(20, 1284, true),
+    {
+      event: `Beta Boulders Winter Pump Fest (Feb 4th) (M)`,
+      ioPercentile: `62.4% (of 140)`,
+    },
+    getIoPercentileForClimbalongCompetition(26, 3381, true),
+    {
+      event: `Beta Boulders Gorilla Unleashed II (Apr 1st) (M)`,
+      ioPercentile: `31.7% (of 115)`,
+    },
+    getIoPercentileForClimbalongCompetition(27, undefined, true),
+  ]);
+};
