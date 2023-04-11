3b2ae3536ea3de7f437b943cbe7065b824ec7b20
klarstrup <io@klarstrup.dk>
2023-04-11 12:44:55 +0200

    remove unused

--- app/page.tsx
+++ app/page.tsx
@@ -91,13 +91,3 @@ const getData = async () => {
     )
   ).sort((a, b) => Number(b.start) - Number(a.start));
 };
-
-function seconds2time(seconds: number) {
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds - hours * 3600) / 60);
-  seconds = seconds - hours * 3600 - minutes * 60;
-
-  return `${hours || "0"}:${minutes < 10 ? `0${minutes}` : String(minutes)}:${
-    seconds < 10 ? `0${seconds}` : String(seconds)
-  }`;
-}
