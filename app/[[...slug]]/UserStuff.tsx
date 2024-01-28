c0c8548e5346b2d894e64c453c4313f9b1e745d0
klarstrup <io@klarstrup.dk>
2024-01-28 14:13:47 +0100

    css popover

--- app/[[...slug]]/UserStuff.tsx
+++ app/[[...slug]]/UserStuff.tsx
@@ -59,49 +59,108 @@ export default async function UserStuff() {
     topLoggerUser = null;
   }
 
-  return currentUser ? (
-    <div>
-      <span>
-        Hello, <strong>{currentUser.name}</strong>
-        <small>({currentUser.email})</small>!
-        {/* eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text */}
-        <img
-          src={currentUser.image || ""}
-          width={50}
-          height={50}
-          style={{ borderRadius: "100%" }}
-        />
-      </span>
-      <p>
-        <a href="/api/auth/signout">Sign out</a>
-      </p>
-      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-      <form action={updateUser}>
-        <fieldset>
-          <legend>Fitocracy Session ID</legend>
-          <input
-            name="fitocracySessionId"
-            defaultValue={currentUser.fitocracySessionId || ""}
-          />
-          {fitocracyProfile ? "✅" : "❌"}
-        </fieldset>
-        <fieldset>
-          <legend>TopLogger ID</legend>
-          <input
-            name="topLoggerId"
-            defaultValue={currentUser.topLoggerId || ""}
-          />
-          {topLoggerUser ? "✅" : "❌"}
-        </fieldset>
-        <input type="submit" value="Update" />
-      </form>
-    </div>
-  ) : (
-    <div>
-      <span>Hello, stranger!</span>
-      <p>
-        <a href="/api/auth/signin">Sign in</a>
-      </p>
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: "4px",
+        right: "4px",
+        paddingLeft: "4px",
+        zIndex: 1337,
+      }}
+    >
+      <style>
+        {`
+        #toggle:checked + div {
+          display: inline-block !important; 
+        }
+        #toggle:checked + div + label {
+          display: inline-block !important; 
+        }
+      `}
+      </style>
+      <label
+        htmlFor="toggle"
+        style={{
+          position: "absolute",
+          top: "4px",
+          right: "4px",
+          zIndex: 1337,
+          cursor: "pointer",
+          userSelect: "none",
+        }}
+      >
+        🌞
+      </label>
+      <input type="checkbox" id="toggle" style={{ display: "none" }} />
+      <div
+        style={{
+          background: "yellow",
+          padding: "8px",
+          display: "none",
+          borderRadius: "10px",
+          boxShadow: "yellow 0px 0px 20px",
+        }}
+      >
+        {currentUser ? (
+          <div>
+            <span>
+              Hello, <strong>{currentUser.name}</strong>
+              <small>({currentUser.email})</small>!
+              {/* eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text */}
+              <img
+                src={currentUser.image || ""}
+                width={50}
+                height={50}
+                style={{ borderRadius: "100%" }}
+              />
+            </span>
+            <p>
+              <a href="/api/auth/signout">Sign out</a>
+            </p>
+            {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+            <form action={updateUser}>
+              <fieldset>
+                <legend>Fitocracy Session ID</legend>
+                <input
+                  name="fitocracySessionId"
+                  defaultValue={currentUser.fitocracySessionId || ""}
+                />
+                {fitocracyProfile ? "✅" : "❌"}
+              </fieldset>
+              <fieldset>
+                <legend>TopLogger ID</legend>
+                <input
+                  name="topLoggerId"
+                  defaultValue={currentUser.topLoggerId || ""}
+                />
+                {topLoggerUser ? "✅" : "❌"}
+              </fieldset>
+              <input type="submit" value="Update" />
+            </form>
+          </div>
+        ) : (
+          <div>
+            <span>Hello, stranger!</span>
+            <p>
+              <a href="/api/auth/signin">Sign in</a>
+            </p>
+          </div>
+        )}
+      </div>
+      <label
+        htmlFor="toggle"
+        style={{
+          display: "none",
+          position: "fixed",
+          zIndex: -1,
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          background: "rgba(0,0,0,0.5)",
+        }}
+      ></label>
     </div>
   );
 }
