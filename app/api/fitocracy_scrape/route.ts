909bd7a69f45306598a6051d7d00b1ce17d4971d
klarstrup <io@klarstrup.dk>
2024-02-05 01:22:23 +0100

    cron scraper

--- app/api/fitocracy_scrape/route.ts
+++ app/api/fitocracy_scrape/route.ts
@@ -1,14 +1,29 @@
+import { UpdateResult } from "mongodb";
+import dbConnect from "../../../dbConnect";
 import { User } from "../../../models/user";
+import { clientPromise } from "../../../mongodb";
 import {
   Fitocracy,
   getUserProfileBySessionId,
   getUserWorkouts,
 } from "../../../sources/fitocracy";
-import { jsonReadableStreamFromAsyncIterable } from "../../../utils";
+import { DAY_IN_SECONDS } from "../../../utils";
+import { NextRequest } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  if (process.env.VERCEL) {
+    const authHeader = request.headers.get("authorization");
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+      return new Response("Unauthorized", {
+        status: 401,
+      });
+    }
+  }
+
+  await dbConnect();
+
   // Io is the only user in the database,
   const user = await User.findOne();
   const fitocracySessionId = user?.fitocracySessionId;
@@ -23,15 +38,57 @@ export async function GET() {
   }
   const fitocracyUserId = fitocracyProfile?.id;
 
-  return new Response(
-    jsonReadableStreamFromAsyncIterable(
-      getUserWorkouts(fitocracySessionId!, fitocracyUserId!, {
-        start: new Date(2001, 9, 11),
-        end: new Date(2012, 9, 11),
-      })
-    ),
-    {
-      headers: { "Content-Type": "application/json" },
+  const responseStream = new TransformStream<Uint8Array, string>();
+  const writer = responseStream.writable.getWriter();
+
+  const workouts = (await clientPromise)
+    .db("test")
+    .collection<Fitocracy.WorkoutData>("fitocracy_workouts");
+
+  (async () => {
+    const workoutsSynchronized: Pick<
+      UpdateResult,
+      "matchedCount" | "modifiedCount" | "upsertedCount"
+    > = {
+      matchedCount: 0,
+      modifiedCount: 0,
+      upsertedCount: 0,
+    };
+
+    const encoder = new TextEncoder();
+
+    await writer.write(encoder.encode("["));
+    let first = true;
+    for await (const workout of getUserWorkouts(
+      fitocracySessionId!,
+      fitocracyUserId!,
+      undefined,
+      { maxAge: DAY_IN_SECONDS }
+    )) {
+      const updateResult = await workouts.updateOne(
+        { id: workout.id },
+        { $set: workout },
+        { upsert: true }
+      );
+      workoutsSynchronized.matchedCount += updateResult.matchedCount;
+      workoutsSynchronized.modifiedCount += updateResult.modifiedCount;
+      workoutsSynchronized.upsertedCount += updateResult.upsertedCount;
+
+      if (first) {
+        first = false;
+      } else {
+        await writer.write(encoder.encode(","));
+      }
+      await writer.write(encoder.encode(JSON.stringify(workout)));
     }
-  );
+    await writer.write(encoder.encode("]"));
+
+    console.log(workoutsSynchronized);
+
+    await writer.close();
+  })().catch(() => {});
+
+  return new Response(responseStream.readable, {
+    headers: { "Content-Type": "application/json" },
+  });
 }
