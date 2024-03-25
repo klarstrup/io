da8e8d3a1133678bb3a2608c31ddff450b2d2476
klarstrup <io@klarstrup.dk>
2024-03-25 23:38:56 +0100

    asd

--- sources/myfitnesspal.ts
+++ sources/myfitnesspal.ts
@@ -142,7 +142,18 @@ export const getMyFitnessPalReport = async (
 ) => {
   const session = await getMyFitnessPalSession(myFitnessPalToken);
   console.log(session);
-
+  console.log("services/diary/report", {
+    method: "POST",
+    body: JSON.stringify({
+      username: session.user?.name,
+      show_food_diary: 1,
+      from: `${year}-01-01`,
+      to: `${year}-12-31`,
+    }),
+    headers: {
+      cookie: "__Secure-next-auth.session-token=" + myFitnessPalToken,
+    },
+  });
   return await fetchMyFitnessPal<MyFitnessPal.ReportEntry[]>(
     "services/diary/report",
     {
