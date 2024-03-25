42a4599a2bf9e5fff855390c0a227e99e7e03317
klarstrup <io@klarstrup.dk>
2024-03-25 23:41:28 +0100

    asdfasdf

--- sources/myfitnesspal.ts
+++ sources/myfitnesspal.ts
@@ -145,7 +145,7 @@ export const getMyFitnessPalReport = async (
   console.log("services/diary/report", {
     method: "POST",
     body: JSON.stringify({
-      username: session.user?.name,
+      username: session.user?.name || "klarstrup",
       show_food_diary: 1,
       from: `${year}-01-01`,
       to: `${year}-12-31`,
