605d3bbf9a5dcf1f3f19a731259f8de2ac56da10
klarstrup <io@klarstrup.dk>
2024-03-29 16:31:41 +0100

    asd

--- app/api/toplogger_scrape/route.ts
+++ app/api/toplogger_scrape/route.ts
@@ -212,7 +212,7 @@ export async function GET(/* request: NextRequest */) {
     const gymIds = new Set<number>();
     console.info(`Upserting ${ascends.length} Io ascends`);
     await Promise.all(
-      randomSlice(ascends, 8).flatMap(({ climb, ...ascend }) => {
+      randomSlice(ascends, 16).flatMap(({ climb, ...ascend }) => {
         gymIds.add(climb.gym_id);
 
         return [
