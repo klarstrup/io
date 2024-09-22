5c401f85ab1d4e659f0a459f546faabd4f4bdd3e
Io Klarstrup <io@klarstrup.dk>
2024-09-23 00:29:00 +0200

    extract NextSets

--- app/diary/DiaryEntryList.tsx
+++ app/diary/DiaryEntryList.tsx
@@ -1,11 +1,144 @@
 import type { Session } from "next-auth";
 import type { DiaryEntry } from "../../lib";
+import { exercises, InputType } from "../../models/exercises";
+import { Workout } from "../../models/workout";
+import { exerciseIdsThatICareAbout } from "../../sources/fitocracy";
 import { MyFitnessPal } from "../../sources/myfitnesspal";
 import WorkoutEntry from "./WorkoutEntry";
 import { WorkoutForm } from "./WorkoutForm";
-import { exerciseIdsThatICareAbout } from "../../sources/fitocracy";
-import { Workout } from "../../models/workout";
-import { exercises, InputType } from "../../models/exercises";
+
+async function NextSets({ user }: { user: Session["user"] }) {
+  return (
+    <div>
+      <b>Next Sets</b>
+      <ol>
+        {(
+          await Promise.all(
+            exerciseIdsThatICareAbout.map(
+              async (id) =>
+                [
+                  id,
+                  (
+                    await Workout.findOne(
+                      {
+                        user_id: user.id,
+                        "exercises.exercise_id": id,
+                        deleted_at: { $exists: false },
+                      },
+                      null,
+                      { sort: { worked_out_at: -1 } }
+                    )
+                  )?.toJSON(),
+                ] as const
+            )
+          )
+        )
+          .map(([id, workout]) => {
+            if (!workout) {
+              return null;
+            }
+
+            const { worked_out_at } = workout;
+            const exercise = workout.exercises.find(
+              ({ exercise_id }) => exercise_id === id
+            )!;
+
+            const heaviestSet = exercise.sets.reduce((acc, set) => {
+              const setWeight = set.inputs.find(
+                (input) => input.type === InputType.Weight
+              )?.value;
+              const accWeight = acc?.inputs.find(
+                (input) => input.type === InputType.Weight
+              )?.value;
+              return setWeight && accWeight && setWeight > accWeight
+                ? set
+                : acc;
+            }, exercise.sets[0]);
+
+            const workingSets = exercise.sets.filter(
+              (set) =>
+                set.inputs.find((input) => input.type === InputType.Weight)
+                  ?.value ===
+                heaviestSet?.inputs.find(
+                  (input) => input.type === InputType.Weight
+                )?.value
+            );
+
+            let successful = true;
+            if (
+              workingSets.length >= 3 ||
+              (exercise.exercise_id === 3 && workingSets.length >= 1)
+            ) {
+              if (
+                workingSets.every(
+                  ({ inputs }) =>
+                    inputs.find(({ type }) => type === InputType.Reps)!.value <
+                    5
+                )
+              ) {
+                successful = false;
+              }
+            } else {
+              successful = false;
+            }
+
+            const nextWorkingSet = successful
+              ? ([1, 183, 532].includes(exercise.exercise_id) ? 1.25 : 2.5) +
+                (heaviestSet?.inputs.find(
+                  (input) => input.type === InputType.Weight
+                )?.value || 0)
+              : 0.9 *
+                (heaviestSet?.inputs.find(
+                  (input) => input.type === InputType.Weight
+                )?.value || 0);
+
+            return {
+              workout_timestamp: worked_out_at,
+              exercise_id: exercise.exercise_id,
+              successful,
+              nextWorkingSet: String(nextWorkingSet).endsWith(".25")
+                ? nextWorkingSet - 0.25
+                : nextWorkingSet,
+            };
+          })
+          .filter(Boolean)
+          .sort(
+            (a, b) =>
+              a.workout_timestamp.getTime() - b.workout_timestamp.getTime()
+          )
+          .map(
+            ({
+              exercise_id,
+              successful,
+              nextWorkingSet,
+              workout_timestamp,
+            }) => {
+              const exercise = exercises.find(({ id }) => exercise_id === id)!;
+
+              return (
+                <li key={exercise.id}>
+                  <b>
+                    {(exercise.aliases[1] || exercise.name).replace(
+                      "Barbell",
+                      ""
+                    )}{" "}
+                    {successful ? null : " (failed)"}
+                  </b>{" "}
+                  {nextWorkingSet}kg{" "}
+                  <small>
+                    <small>
+                      Last set{" "}
+                      {String(workout_timestamp.toLocaleDateString("da-DK"))}
+                    </small>
+                  </small>
+                </li>
+              );
+            }
+          )}
+      </ol>
+    </div>
+  );
+}
 
 export function DiaryEntryList({
   diaryEntries,
@@ -17,7 +150,7 @@ export function DiaryEntryList({
   const todayStr = `${new Date().getFullYear()}-${
     new Date().getMonth() + 1
   }-${new Date().getDate()}`;
-  return diaryEntries.map(async ([date, { food, workouts }]) => {
+  return diaryEntries.map(([date, { food, workouts }]) => {
     const dayTotalEnergy = food?.reduce(
       (acc, foodEntry) => acc + foodEntry.nutritional_contents.energy.value,
       0
@@ -167,141 +300,7 @@ export function DiaryEntryList({
           <fieldset>
             <legend>New workout</legend>
             <WorkoutForm user={user} />
-            <div>
-              <b>Next Sets</b>
-              <ol>
-                {(
-                  await Promise.all(
-                    exerciseIdsThatICareAbout.map(
-                      async (id) =>
-                        [
-                          id,
-                          (
-                            await Workout.findOne(
-                              {
-                                "exercises.exercise_id": id,
-                                deleted_at: { $exists: false },
-                              },
-                              null,
-                              { sort: { workout_timestamp: -1 } }
-                            )
-                          )?.toJSON(),
-                        ] as const
-                    )
-                  )
-                )
-                  .map(([id, workout]) => {
-                    if (!workout) {
-                      return null;
-                    }
-
-                    const { worked_out_at } = workout;
-                    const exercise = workout.exercises.find(
-                      ({ exercise_id }) => exercise_id === id
-                    )!;
-
-                    const heaviestSet = exercise.sets.reduce((acc, set) => {
-                      const setWeight = set.inputs.find(
-                        (input) => input.type === InputType.Weight
-                      )?.value;
-                      const accWeight = acc?.inputs.find(
-                        (input) => input.type === InputType.Weight
-                      )?.value;
-                      return setWeight && accWeight && setWeight > accWeight
-                        ? set
-                        : acc;
-                    }, exercise.sets[0]);
-
-                    const workingSets = exercise.sets.filter(
-                      (set) =>
-                        set.inputs.find(
-                          (input) => input.type === InputType.Weight
-                        )?.value ===
-                        heaviestSet?.inputs.find(
-                          (input) => input.type === InputType.Weight
-                        )?.value
-                    );
-
-                    let successful = true;
-                    if (
-                      workingSets.length >= 3 ||
-                      (exercise.exercise_id === 3 && workingSets.length >= 1)
-                    ) {
-                      if (
-                        workingSets.every(
-                          ({ inputs }) =>
-                            inputs.find(({ type }) => type === InputType.Reps)!
-                              .value < 5
-                        )
-                      ) {
-                        successful = false;
-                      }
-                    } else {
-                      successful = false;
-                    }
-
-                    const nextWorkingSet = successful
-                      ? ([1, 183, 532].includes(exercise.exercise_id)
-                          ? 1.25
-                          : 2.5) +
-                        (heaviestSet?.inputs.find(
-                          (input) => input.type === InputType.Weight
-                        )?.value || 0)
-                      : 0.9 *
-                        (heaviestSet?.inputs.find(
-                          (input) => input.type === InputType.Weight
-                        )?.value || 0);
-
-                    return {
-                      workout_timestamp: worked_out_at,
-                      exercise_id: exercise.exercise_id,
-                      successful,
-                      nextWorkingSet: String(nextWorkingSet).endsWith(".25")
-                        ? nextWorkingSet - 0.25
-                        : nextWorkingSet,
-                    };
-                  })
-                  .filter(Boolean)
-                  .sort(
-                    (a, b) =>
-                      a.workout_timestamp.getTime() -
-                      b.workout_timestamp.getTime()
-                  )
-                  .map(
-                    ({
-                      exercise_id,
-                      successful,
-                      nextWorkingSet,
-                      workout_timestamp,
-                    }) => {
-                      const exercise = exercises.find(
-                        ({ id }) => exercise_id === id
-                      )!;
-
-                      return (
-                        <li key={exercise.id}>
-                          <b>
-                            {(exercise.aliases[1] || exercise.name).replace(
-                              "Barbell",
-                              ""
-                            )}{" "}
-                            {successful ? null : " (failed)"}
-                          </b>{" "}
-                          {nextWorkingSet}kg{" "}
-                          <small>
-                            <small>
-                              Last set{" "}
-                              {String(
-                                workout_timestamp.toLocaleDateString("da-DK")
-                              )}
-                            </small>
-                          </small>
-                        </li>
-                      );
-                    }
-                  )}
-              </ol>
-            </div>
+            <NextSets user={user} />
           </fieldset>
         ) : null}
         <hr />
