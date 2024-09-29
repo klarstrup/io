09017a73718519ab3edf2db821233633049b84c2
Io Klarstrup <io@klarstrup.dk>
2024-09-29 20:22:40 +0200

    + dropdown for workouts and food

--- app/diary/DiaryEntryItem.tsx
+++ app/diary/DiaryEntryItem.tsx
@@ -2,6 +2,7 @@
 import { TZDate } from "@date-fns/tz";
 import { differenceInDays } from "date-fns";
 import type { Session } from "next-auth";
+import { useState } from "react";
 import type { DiaryEntry } from "../../lib";
 import type { getNextSets } from "../../models/workout.server";
 import { MyFitnessPal } from "../../sources/myfitnesspal";
@@ -20,6 +21,8 @@ export function DiaryEntryItem({
   locations: string[];
   nextSets: Awaited<ReturnType<typeof getNextSets>>;
 }) {
+  const [isAddingWorkout, setIsAddingWorkout] = useState(false);
+
   const now = TZDate.tz("Europe/Copenhagen");
   const todayStr = `${now.getFullYear()}-${
     now.getMonth() + 1
@@ -54,104 +57,132 @@ export function DiaryEntryItem({
       <div
         style={{
           marginBottom: "0.5em",
-          marginLeft: "1em",
+          marginLeft: "0.5em",
+          display: "flex",
+          alignItems: "center",
         }}
       >
-        <big>{date}</big>{" "}
-        {dayTotalEnergy && dayTotalProtein ? (
-          <small>
-            {Math.round(dayTotalEnergy)} kcal, {Math.round(dayTotalProtein)}g
-            protein
-          </small>
-        ) : null}
+        <div style={{ flex: 1, lineHeight: 1, display: "flex" }}>
+          <big>
+            <big>
+              <big>{date}</big>
+            </big>
+          </big>
+          {dayTotalEnergy && dayTotalProtein ? (
+            <small style={{ paddingLeft: "0.5em" }}>
+              <div>{Math.round(dayTotalEnergy)} kcal</div>
+              <div>{Math.round(dayTotalProtein)}g protein</div>
+            </small>
+          ) : null}
+        </div>
+        <select
+          onChange={(e) => {
+            if (e.target.value === "workout") {
+              setIsAddingWorkout(true);
+            }
+            if (e.target.value === "food") {
+              window.open(
+                `https://www.myfitnesspal.com/food/diary?date=${date}`
+              );
+            }
+
+            e.target.value = "";
+          }}
+          style={{
+            fontSize: "1em",
+            padding: "0.5em 0em",
+            paddingRight: "0em",
+            borderRadius: "1em",
+            border: "none",
+            background: "#ff0",
+            textAlign: "center",
+            maxWidth: "3em",
+          }}
+        >
+          <option value="">➕</option>
+          <option value="workout">Add Workout</option>
+          <option value="food">Log Food</option>
+        </select>
       </div>
       <div style={{ flex: "1" }}>
         <div>
           {food ? (
-            <>
-              <small>Meals:</small>
-              <ol style={{ paddingInlineStart: "20px" }}>
-                {[
-                  MyFitnessPal.MealName.Breakfast,
-                  MyFitnessPal.MealName.Lunch,
-                  MyFitnessPal.MealName.Dinner,
-                  MyFitnessPal.MealName.Snacks,
-                ]
-                  .filter((mealName) =>
-                    food?.some((foodEntry) => foodEntry.meal_name === mealName)
-                  )
-                  .map((mealName) => {
-                    const mealTotalEnergy = food
-                      ?.filter((foodEntry) => foodEntry.meal_name === mealName)
-                      .reduce(
-                        (acc, foodEntry) =>
-                          acc + foodEntry.nutritional_contents.energy.value,
-                        0
-                      );
-                    const mealTotalProtein = food
-                      ?.filter((foodEntry) => foodEntry.meal_name === mealName)
-                      .reduce(
-                        (acc, foodEntry) =>
-                          acc + (foodEntry.nutritional_contents.protein || 0),
-                        0
-                      );
-
-                    return (
-                      <li key={mealName}>
-                        <div>
-                          <b>{mealName}</b>{" "}
-                          {mealTotalEnergy && mealTotalProtein ? (
-                            <small>
-                              {Math.round(mealTotalEnergy)} kcal,{" "}
-                              {Math.round(mealTotalProtein)}g protein
-                            </small>
-                          ) : null}
-                        </div>
-                        <ul style={{ paddingInlineStart: "0px" }}>
-                          {food
-                            ?.filter(
-                              (foodEntry) => foodEntry.meal_name === mealName
-                            )
-                            .map((foodEntry) => (
-                              <li key={foodEntry.id}>
-                                {foodEntry.food.description.replace(
-                                  /\s+/g,
-                                  " "
-                                )}
-                                <small>
-                                  {" "}
-                                  {foodEntry.servings *
-                                    foodEntry.serving_size.value !==
-                                  1 ? (
-                                    <>
-                                      {Math.round(
-                                        foodEntry.servings *
-                                          foodEntry.serving_size.value
-                                      )}{" "}
-                                      {foodEntry.serving_size.unit.match(
-                                        /container/i
-                                      )
-                                        ? "x "
-                                        : null}
-                                    </>
-                                  ) : null}
-                                  {foodEntry.serving_size.unit.match(
-                                    /container/i
-                                  )
-                                    ? foodEntry.serving_size.unit.replace(
-                                        /(container \((.+)\))/i,
-                                        "$2"
-                                      )
-                                    : foodEntry.serving_size.unit}
-                                </small>
-                              </li>
-                            ))}
-                        </ul>
-                      </li>
+            <ol style={{ paddingInlineStart: "20px" }}>
+              {[
+                MyFitnessPal.MealName.Breakfast,
+                MyFitnessPal.MealName.Lunch,
+                MyFitnessPal.MealName.Dinner,
+                MyFitnessPal.MealName.Snacks,
+              ]
+                .filter((mealName) =>
+                  food?.some((foodEntry) => foodEntry.meal_name === mealName)
+                )
+                .map((mealName) => {
+                  const mealTotalEnergy = food
+                    ?.filter((foodEntry) => foodEntry.meal_name === mealName)
+                    .reduce(
+                      (acc, foodEntry) =>
+                        acc + foodEntry.nutritional_contents.energy.value,
+                      0
                     );
-                  })}
-              </ol>
-            </>
+                  const mealTotalProtein = food
+                    ?.filter((foodEntry) => foodEntry.meal_name === mealName)
+                    .reduce(
+                      (acc, foodEntry) =>
+                        acc + (foodEntry.nutritional_contents.protein || 0),
+                      0
+                    );
+
+                  return (
+                    <li key={mealName}>
+                      <div>
+                        <b>{mealName}</b>{" "}
+                        {mealTotalEnergy && mealTotalProtein ? (
+                          <small>
+                            {Math.round(mealTotalEnergy)} kcal,{" "}
+                            {Math.round(mealTotalProtein)}g protein
+                          </small>
+                        ) : null}
+                      </div>
+                      <ul style={{ paddingInlineStart: "0px" }}>
+                        {food
+                          ?.filter(
+                            (foodEntry) => foodEntry.meal_name === mealName
+                          )
+                          .map((foodEntry) => (
+                            <li key={foodEntry.id}>
+                              {foodEntry.food.description.replace(/\s+/g, " ")}
+                              <small>
+                                {" "}
+                                {foodEntry.servings *
+                                  foodEntry.serving_size.value !==
+                                1 ? (
+                                  <>
+                                    {Math.round(
+                                      foodEntry.servings *
+                                        foodEntry.serving_size.value
+                                    )}{" "}
+                                    {foodEntry.serving_size.unit.match(
+                                      /container/i
+                                    )
+                                      ? "x "
+                                      : null}
+                                  </>
+                                ) : null}
+                                {foodEntry.serving_size.unit.match(/container/i)
+                                  ? foodEntry.serving_size.unit.replace(
+                                      /(container \((.+)\))/i,
+                                      "$2"
+                                    )
+                                  : foodEntry.serving_size.unit}
+                              </small>
+                            </li>
+                          ))}
+                      </ul>
+                    </li>
+                  );
+                })}
+            </ol>
           ) : null}
         </div>
         <div>
@@ -165,37 +196,33 @@ export function DiaryEntryItem({
                 />
               ))
             : null}
+          {isAddingWorkout ? (
+            <fieldset>
+              <legend>New workout</legend>
+              <WorkoutForm
+                user={user}
+                locations={locations}
+                nextSets={nextSets}
+                onClose={() => setIsAddingWorkout(false)}
+              />
+              <div>
+                <small>
+                  <b>Future Sets:</b>
+                  <NextSets
+                    nextSets={nextSets.filter(
+                      (nextSet) =>
+                        differenceInDays(
+                          new Date(),
+                          nextSet.workout_timestamp
+                        ) <= 2
+                    )}
+                  />
+                </small>
+              </div>
+            </fieldset>
+          ) : null}
         </div>
       </div>
-      {date === todayStr ? (
-        <fieldset>
-          <legend>New workout</legend>
-          <WorkoutForm user={user} locations={locations} nextSets={nextSets} />
-          <div>
-            <small>
-              <b>Future Sets:</b>
-              <NextSets
-                nextSets={nextSets.filter(
-                  (nextSet) =>
-                    differenceInDays(new Date(), nextSet.workout_timestamp) <= 2
-                )}
-              />
-            </small>
-          </div>
-        </fieldset>
-      ) : null}
-      <hr />
-      <small
-        style={{
-          textAlign: "center",
-          display: "flex",
-          justifyContent: "space-evenly",
-          opacity: 0.5,
-        }}
-      >
-        {!food?.length ? <i>No meals logged</i> : null}
-        {!workouts?.length ? <i>No lifts logged</i> : null}
-      </small>
     </div>
   );
 }
