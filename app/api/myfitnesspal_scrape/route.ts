3710bbbcd59a69a41b634c93fea03aa1faef740e
Io Klarstrup <io@klarstrup.dk>
2024-10-19 18:23:49 +0900

    Refactor GET function in myfitnesspal_scrape/route.ts to use jsonStreamResponse helper

--- app/api/myfitnesspal_scrape/route.ts
+++ app/api/myfitnesspal_scrape/route.ts
@@ -2,8 +2,9 @@ import { differenceInMonths, isFuture } from "date-fns";
 import { DateTime } from "luxon";
 import { auth } from "../../../auth";
 import { getDB } from "../../../dbConnect";
-import { MyFitnessPal } from "../../../sources/myfitnesspal";
+import type { MyFitnessPal } from "../../../sources/myfitnesspal";
 import { getMyFitnessPalReport } from "../../../sources/myfitnesspal.server";
+import { jsonStreamResponse } from "../scraper-utils";
 
 const months = [
   "01",
@@ -25,38 +26,24 @@ const years = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024] as const;
 export const dynamic = "force-dynamic";
 export const maxDuration = 60;
 
-export async function GET() {
-  const user = (await auth())?.user;
-  if (!user) return new Response("Unauthorized", { status: 401 });
+export const GET = () =>
+  jsonStreamResponse(async function* () {
+    const user = (await auth())?.user;
+    if (!user) return new Response("Unauthorized", { status: 401 });
 
-  const myFitnessPalToken = user.myFitnessPalToken;
-  if (!myFitnessPalToken) {
-    return new Response("No myFitnessPalToken", { status: 401 });
-  }
+    const token = user.myFitnessPalToken;
+    if (!token) return new Response("No token", { status: 401 });
 
-  const myFitnessPalUserId = user.myFitnessPalUserId;
-  if (!myFitnessPalUserId) {
-    return new Response("No myFitnessPalUserId", { status: 401 });
-  }
+    const userId = user.myFitnessPalUserId;
+    if (!userId) return new Response("No userId", { status: 401 });
 
-  const myFitnessPalUserName = user.myFitnessPalUserName;
-  if (!myFitnessPalUserName) {
-    return new Response("No myFitnessPalUserName", { status: 401 });
-  }
+    const userName = user.myFitnessPalUserName;
+    if (!userName) return new Response("No userName", { status: 401 });
 
-  const responseStream = new TransformStream<Uint8Array, string>();
-  const writer = responseStream.writable.getWriter();
-
-  const DB = await getDB();
-  const foodEntries = DB.collection<MyFitnessPal.MongoFoodEntry>(
-    "myfitnesspal_food_entries",
-  );
-
-  (async () => {
-    const encoder = new TextEncoder();
-
-    await writer.write(encoder.encode("[\n"));
-    let first = true;
+    const DB = await getDB();
+    const foodEntries = DB.collection<MyFitnessPal.MongoFoodEntry>(
+      "myfitnesspal_food_entries",
+    );
 
     const now = new Date();
     yearLoop: for (const year of years) {
@@ -65,15 +52,15 @@ export async function GET() {
 
         if (differenceInMonths(now, new Date(year, Number(month) - 1)) > 1) {
           const entriesForMonth = await foodEntries.countDocuments({
-            user_id: myFitnessPalUserId,
+            user_id: userId,
             date: { $regex: new RegExp(`^${year}-${month}-`) },
           });
 
           if (entriesForMonth > 0) continue;
         }
         const reportEntries = await getMyFitnessPalReport(
-          myFitnessPalToken,
-          myFitnessPalUserName,
+          token,
+          userName,
           year,
           month,
         );
@@ -81,7 +68,7 @@ export async function GET() {
         if (Array.isArray(reportEntries)) {
           // Wipe the month to be replaced with the new data
           await foodEntries.deleteMany({
-            user_id: myFitnessPalUserId,
+            user_id: userId,
             date: { $regex: new RegExp(`^${year}-${month}-`) },
           });
         }
@@ -94,7 +81,7 @@ export async function GET() {
                 {
                   $set: {
                     ...foodEntry,
-                    user_id: myFitnessPalUserId,
+                    user_id: userId,
                     datetime: DateTime.fromISO(foodEntry.date, {
                       zone: "utc",
                     }).toJSDate(),
@@ -103,24 +90,10 @@ export async function GET() {
                 { upsert: true },
               );
             }
-            if (first) {
-              first = false;
-            } else {
-              await writer.write(encoder.encode(",\n"));
-            }
-            await writer.write(
-              encoder.encode(JSON.stringify(reportEntry.date)),
-            );
+
+            yield reportEntry.date;
           }
         }
       }
     }
-    await writer.write(encoder.encode("\n]"));
-
-    await writer.close();
-  })().catch(() => {});
-
-  return new Response(responseStream.readable, {
-    headers: { "Content-Type": "application/json" },
   });
-}
