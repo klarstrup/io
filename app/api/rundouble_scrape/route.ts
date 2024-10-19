e37b0b519923fcf856a9bfd1576f0397fbb1ff0d
Io Klarstrup <io@klarstrup.dk>
2024-10-19 18:16:14 +0900

    Refactor GET function in rundouble_scrape/route.ts to use jsonStreamResponse helper

--- app/api/rundouble_scrape/route.ts
+++ app/api/rundouble_scrape/route.ts
@@ -2,32 +2,23 @@ import { auth } from "../../../auth";
 import { getDB } from "../../../dbConnect";
 import type { ScrapedAt } from "../../../lib";
 import { getRuns, type RunDouble } from "../../../sources/rundouble";
+import { jsonStreamResponse } from "../scraper-utils";
 
 export const dynamic = "force-dynamic";
 export const maxDuration = 60;
 
-export async function GET() {
-  const user = (await auth())?.user;
-  if (!user) return new Response("Unauthorized", { status: 401 });
+export const GET = () =>
+  jsonStreamResponse(async function* () {
+    const user = (await auth())?.user;
+    if (!user) return new Response("Unauthorized", { status: 401 });
 
-  const runDoubleId = user.runDoubleId;
-  if (!runDoubleId) {
-    return new Response("No runDoubleId", { status: 401 });
-  }
+    const runDoubleId = user.runDoubleId;
+    if (!runDoubleId) return new Response("No runDoubleId", { status: 401 });
 
-  const responseStream = new TransformStream<Uint8Array, string>();
-  const writer = responseStream.writable.getWriter();
-
-  const DB = await getDB();
-  const runsCollection = DB.collection<RunDouble.MongoHistoryItem & ScrapedAt>(
-    "rundouble_runs",
-  );
-
-  (async () => {
-    const encoder = new TextEncoder();
-
-    await writer.write(encoder.encode("[\n"));
-    let first = true;
+    const DB = await getDB();
+    const runsCollection = DB.collection<
+      RunDouble.MongoHistoryItem & ScrapedAt
+    >("rundouble_runs");
 
     for await (const run of getRuns(runDoubleId, { maxAge: 0 })) {
       const updateResult = await runsCollection.updateOne(
@@ -43,24 +34,8 @@ export async function GET() {
         { upsert: true },
       );
 
-      if (first) {
-        first = false;
-      } else {
-        await writer.write(encoder.encode(",\n"));
-      }
-      await writer.write(encoder.encode(JSON.stringify(run.completed)));
+      yield run.completed;
 
-      if (!updateResult.upsertedCount) {
-        break;
-      }
+      if (!updateResult.upsertedCount) break;
     }
-
-    await writer.write(encoder.encode("\n]"));
-
-    await writer.close();
-  })().catch(() => {});
-
-  return new Response(responseStream.readable, {
-    headers: { "Content-Type": "application/json" },
   });
-}
