54214664f1219bb2e27eac0da34dd351c284c901
Io Klarstrup <io@klarstrup.dk>
2024-10-20 16:35:16 +0900

    extract DiaryAgendaWorkouts and make DiaryAgenda a server component

--- app/diary/DiaryAgenda.tsx
+++ app/diary/DiaryAgenda.tsx
@@ -1,9 +1,6 @@
-"use client";
 import { tz, TZDate } from "@date-fns/tz";
 import {
   addDays,
-  compareAsc,
-  differenceInDays,
   differenceInHours,
   eachDayOfInterval,
   endOfDay,
@@ -15,7 +12,6 @@ import {
 } from "date-fns";
 import type { Session } from "next-auth";
 import Image from "next/image";
-import { useState } from "react";
 import * as weatherIconsByCode from "../../components/weather-icons/index";
 import type {
   DiaryEntry,
@@ -30,11 +26,8 @@ import {
   getSunset,
   roundToNearestDay,
 } from "../../utils";
-import { EntryAdder } from "./EntryAdder";
+import { DiaryAgendaWorkouts } from "./DiaryAgendaWorkouts";
 import { FoodEntry } from "./FoodEntry";
-import { NextSets } from "./NextSets";
-import WorkoutEntry from "./WorkoutEntry";
-import { WorkoutForm } from "./WorkoutForm";
 
 export function DiaryAgenda({
   diaryEntry,
@@ -53,8 +46,6 @@ export function DiaryAgenda({
 }) {
   const timeZone = user.timeZone || DEFAULT_TIMEZONE;
 
-  const [isAddingWorkout, setIsAddingWorkout] = useState(false);
-
   const [date, { food, workouts }] = diaryEntry;
 
   const dayTotalEnergy = food?.reduce(
@@ -67,9 +58,6 @@ export function DiaryAgenda({
   );
 
   const now = TZDate.tz(timeZone);
-  const dueSets = nextSets?.filter(
-    (nextSet) => differenceInDays(startOfDay(now), nextSet.workedOutAt) > 3,
-  );
   const userLocation = user.geohash ? decodeGeohash(user.geohash) : null;
   const sunrise = getSunrise(
     userLocation?.latitude ?? 55.658693,
@@ -111,11 +99,6 @@ export function DiaryAgenda({
             </big>
           </big>
         </div>
-        <EntryAdder
-          diaryEntry={diaryEntry}
-          user={user}
-          onAddWorkout={() => setIsAddingWorkout(true)}
-        />
       </div>
       <div className="flex flex-1 flex-wrap gap-2">
         <div className="flex flex-[2] flex-col">
@@ -135,70 +118,24 @@ export function DiaryAgenda({
               <div className="flex h-full flex-col items-center justify-center">
                 <p className="mb-2">No food logged</p>
                 <div>
-                  <button
-                    onClick={() => {
-                      window.open(
-                        `https://www.myfitnesspal.com/food/diary?date=${date}`,
-                      );
-                    }}
-                    className="mb-4 cursor-pointer rounded-2xl bg-[#ff0] px-3 py-2 pr-4 text-center text-xl font-semibold"
+                  <a
+                    href={`https://www.myfitnesspal.com/food/diary?date=${date}`}
+                    target="_blank"
+                    className="mb-4 inline-block cursor-pointer rounded-2xl bg-[#ff0] px-3 py-2 pr-4 text-center text-xl font-semibold"
                   >
                     ➕ Food
-                  </button>
+                  </a>
                 </div>
               </div>
             )}
           </fieldset>
-          <fieldset className="flex-1 rounded-lg border-x-0 border-y-4 border-gray-200 px-1 py-2">
-            <legend className="ml-2">
-              <big>Workouts</big>
-            </legend>
-            {isAddingWorkout ? null : workouts?.length ? (
-              Array.from(workouts)
-                .sort((a, b) => compareAsc(a.workedOutAt, b.workedOutAt))
-                ?.map((workout) => (
-                  <WorkoutEntry
-                    key={workout._id}
-                    user={user}
-                    workout={workout}
-                    locations={locations}
-                    nextSets={nextSets}
-                  />
-                ))
-            ) : (
-              <div className="flex h-full flex-wrap items-center justify-around">
-                <div className="flex flex-col items-center justify-center">
-                  <p className="mb-2">No workout logged </p>
-                  <div>
-                    <button
-                      onClick={() => setIsAddingWorkout(true)}
-                      className="mb-4 cursor-pointer rounded-2xl bg-[#ff0] px-3 py-2 pr-4 text-center text-xl font-semibold"
-                    >
-                      ➕ Workout
-                    </button>
-                  </div>
-                </div>
-                {dueSets?.length ? (
-                  <div>
-                    <b>Due Sets:</b>
-                    <NextSets nextSets={dueSets} />
-                  </div>
-                ) : null}
-              </div>
-            )}
-            {isAddingWorkout ? (
-              <fieldset className="w-full">
-                <legend>New workout</legend>
-                <WorkoutForm
-                  date={date}
-                  user={user}
-                  locations={locations}
-                  nextSets={nextSets}
-                  onClose={() => setIsAddingWorkout(false)}
-                />
-              </fieldset>
-            ) : null}
-          </fieldset>
+          <DiaryAgendaWorkouts
+            date={date}
+            workouts={workouts}
+            user={user}
+            locations={locations}
+            nextSets={nextSets}
+          />
         </div>
         {calendarEvents?.length ? (
           <fieldset className="flex flex-1 flex-col rounded-lg border-x-0 border-y-4 border-gray-200 px-1 py-2">
---
+++ app/diary/DiaryAgendaWorkouts.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { TZDate } from "@date-fns/tz";
+import { compareAsc, differenceInDays, startOfDay } from "date-fns";
+import type { Session } from "next-auth";
+import { useState } from "react";
+import type { DiaryEntry } from "../../lib";
+import type { getNextSets } from "../../models/workout.server";
+import { DEFAULT_TIMEZONE } from "../../utils";
+import { NextSets } from "./NextSets";
+import WorkoutEntry from "./WorkoutEntry";
+import { WorkoutForm } from "./WorkoutForm";
+
+export function DiaryAgendaWorkouts({
+  date,
+  workouts,
+  user,
+  locations,
+  nextSets,
+}: {
+  date: `${number}-${number}-${number}`;
+  workouts: DiaryEntry["workouts"];
+  user: Session["user"];
+  locations: string[];
+  nextSets: Awaited<ReturnType<typeof getNextSets>>;
+}) {
+  const timeZone = user.timeZone || DEFAULT_TIMEZONE;
+
+  const [isAddingWorkout, setIsAddingWorkout] = useState(false);
+
+  const now = TZDate.tz(timeZone);
+  const dueSets = nextSets?.filter(
+    (nextSet) => differenceInDays(startOfDay(now), nextSet.workedOutAt) > 3,
+  );
+
+  return (
+    <fieldset className="flex-1 rounded-lg border-x-0 border-y-4 border-gray-200 px-1 py-2">
+      <legend className="ml-2">
+        <big>Workouts</big>
+      </legend>
+      {isAddingWorkout ? null : workouts?.length ? (
+        Array.from(workouts)
+          .sort((a, b) => compareAsc(a.workedOutAt, b.workedOutAt))
+          ?.map((workout) => (
+            <WorkoutEntry
+              key={workout._id}
+              user={user}
+              workout={workout}
+              locations={locations}
+              nextSets={nextSets}
+            />
+          ))
+      ) : (
+        <div className="flex h-full flex-wrap items-center justify-around">
+          <div className="flex flex-col items-center justify-center">
+            <p className="mb-2">No workout logged </p>
+            <div>
+              <button
+                onClick={() => setIsAddingWorkout(true)}
+                className="mb-4 cursor-pointer rounded-2xl bg-[#ff0] px-3 py-2 pr-4 text-center text-xl font-semibold"
+              >
+                ➕ Workout
+              </button>
+            </div>
+          </div>
+          {dueSets?.length ? (
+            <div>
+              <b>Due Sets:</b>
+              <NextSets nextSets={dueSets} />
+            </div>
+          ) : null}
+        </div>
+      )}
+      {isAddingWorkout ? (
+        <fieldset className="w-full">
+          <legend>New workout</legend>
+          <WorkoutForm
+            date={date}
+            user={user}
+            locations={locations}
+            nextSets={nextSets}
+            onClose={() => setIsAddingWorkout(false)}
+          />
+        </fieldset>
+      ) : null}
+    </fieldset>
+  );
+}
