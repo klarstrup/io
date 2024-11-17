1bf06ffb195b55f390f2cf8d68d13f0c45a77616
Io Klarstrup <io@klarstrup.dk>
2024-11-18 00:40:01 +0100

    fix: correct type check for date string parsing

--- app/api/toplogger_gql_scrape/route.ts
+++ app/api/toplogger_gql_scrape/route.ts
@@ -333,7 +333,7 @@ const dereferenceDocument = async <
 const parseDateFields = (doc: Record<string, unknown>) => {
   for (const key in doc) {
     const value = doc[key];
-    if (value === "string") {
+    if (typeof value === "string") {
       if (value.match(/^\d{4}-\d{2}-\d{2}/)) {
         doc[key] = TZDate.tz("Etc/UTC", value);
       }
