ad3c9c25da2cea8ca784e662c1080d62ac579dc2
Io Klarstrup <io@klarstrup.dk>
2024-11-23 01:15:20 +0100

    Move difficultyToGradeMap to sources/kilterboard.ts for better organization

--- app/api/kilterboard_scrape/route.ts
+++ app/api/kilterboard_scrape/route.ts
@@ -1,5 +1,6 @@
 import { auth } from "../../../auth";
 import {
+  difficultyToGradeMap,
   type KilterBoard,
   KilterBoardAscents,
 } from "../../../sources/kilterboard";
@@ -7,49 +8,6 @@ import { jsonStreamResponse } from "../scraper-utils";
 
 export const dynamic = "force-dynamic";
 export const maxDuration = 60;
-
-// Approximate mapping of KilterBoard "difficulty" to TopLogger "grade"
-export const difficultyToGradeMap = {
-  1: 1,
-  2: 1.33,
-  3: 1.67,
-  4: 2,
-  5: 2.33,
-  6: 2.67,
-  7: 3,
-  8: 3.33,
-  9: 3.67,
-  10: 4,
-  11: 4.33,
-  12: 4.67,
-  13: 5,
-  14: 5.33,
-  15: 5.67,
-  16: 6,
-  17: 6.17,
-  18: 6.33,
-  19: 6.5,
-  20: 6.67,
-  21: 6.83,
-  22: 7,
-  23: 7.17,
-  24: 7.33,
-  25: 7.5,
-  26: 7.67,
-  27: 7.83,
-  28: 8,
-  29: 8.17,
-  30: 8.33,
-  31: 8.5,
-  32: 8.67,
-  33: 8.83,
-  34: 9,
-  35: 9.17,
-  36: 9.33,
-  37: 9.5,
-  38: 9.67,
-  39: 9.83,
-} as const;
 
 export const GET = () =>
   // eslint-disable-next-line require-yield
