f67062cdbb3c2a15ee1ffd79728fc2ffeb7d844e
Io Klarstrup <io@klarstrup.dk>
2024-12-04 22:28:38 +0100

    Refactor cron route to dynamically select data source based on last attempted scrape time

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,9 +1,37 @@
 import { redirect } from "next/navigation";
-import { scraperEndpoints } from "../scraper-utils";
+import { Users } from "../../../models/user.server";
+import { DataSource, UserDataSource } from "../../../sources/utils";
+import { compareAsc } from "date-fns";
 
-export function GET() {
-  const endoint =
-    scraperEndpoints[Math.floor(Math.random() * scraperEndpoints.length)]!;
+const sourceScrapeMap: Omit<
+  Record<DataSource, string>,
+  DataSource.Fitocracy
+> = {
+  ical: "ical_scrape",
+  myfitnesspal: "myfitnesspal_scrape",
+  rundouble: "rundouble_scrape",
+  tomorrow: "tomorrow_scrape",
+  toplogger: "toplogger_gql_scrape",
+  kilterboard: "kilterboard_scrape",
+};
 
-  redirect(`/api/${endoint}`);
+export async function GET() {
+  const users = await Users.find({}).toArray();
+
+  const dataSources = users
+    .flatMap((user) => user.dataSources)
+    .filter((dataSource): dataSource is UserDataSource => Boolean(dataSource))
+    .filter((dataSource) => dataSource.source !== DataSource.Fitocracy) // Fitocracy is read-only
+    .sort((a, b) =>
+      compareAsc(
+        a.lastAttemptedAt ?? new Date(0),
+        b.lastAttemptedAt ?? new Date(0),
+      ),
+    );
+
+  const leastRecentlyAttempted = dataSources[0];
+
+  if (!leastRecentlyAttempted) return Response.json({});
+
+  redirect(`/api/${sourceScrapeMap[leastRecentlyAttempted.source]}`);
 }
