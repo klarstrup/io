c6ac6d9e6ad0515a648c83ccdf0465bdd8df4a1c
Io Klarstrup <io@klarstrup.dk>
2024-12-04 11:11:48 +0100

    Refactor ical_scrape route to utilize wrapSource for improved data handling and streamline event processing logic

--- app/api/ical_scrape/route.ts
+++ app/api/ical_scrape/route.ts
@@ -1,14 +1,13 @@
 import { createHash } from "crypto";
-import { ObjectId } from "mongodb";
 import { auth } from "../../../auth";
 import type { IcalIoMeta } from "../../../lib";
-import { Users } from "../../../models/user.server";
 import {
   extractIcalCalendarAndEvents,
   fetchAndParseIcal,
 } from "../../../sources/ical";
 import { IcalEvents } from "../../../sources/ical.server";
 import { DataSource } from "../../../sources/utils";
+import { wrapSource } from "../../../sources/utils.server";
 import { jsonStreamResponse } from "../scraper-utils";
 
 export const dynamic = "force-dynamic";
@@ -23,68 +22,48 @@ export const GET = () =>
       (source) => source.source === DataSource.ICal,
     );
 
-    for (const {
-      id,
-      config: { url },
-    } of userIcalSources ?? []) {
-      await Users.updateOne(
-        { _id: new ObjectId(user.id) },
-        { $set: { "dataSources.$[source].lastAttemptedAt": new Date() } },
-        { arrayFilters: [{ "source.id": id }] },
-      );
-      const runtime = Date.now();
+    for (const dataSource of userIcalSources ?? []) {
+      yield* wrapSource(dataSource, user, async function* () {
+        const icalData = await fetchAndParseIcal(dataSource.config.url);
 
-      const icalData = await fetchAndParseIcal(url);
+        const icalUrlHash = createHash("sha256")
+          .update(dataSource.config.url + user.id)
+          .digest("hex");
 
-      const icalUrlHash = createHash("sha256")
-        .update(url + user.id)
-        .digest("hex");
+        const _io_scrapedAt = new Date();
+        const ioIcalMeta: IcalIoMeta = {
+          _io_userId: user.id,
+          _io_icalUrlHash: icalUrlHash,
+        };
+        const { calendar, events } = extractIcalCalendarAndEvents(icalData);
 
-      const _io_scrapedAt = new Date();
-      const ioIcalMeta: IcalIoMeta = {
-        _io_userId: user.id,
-        _io_icalUrlHash: icalUrlHash,
-      };
-      const { calendar, events } = extractIcalCalendarAndEvents(icalData);
+        const existingEventsCount = await IcalEvents.countDocuments(ioIcalMeta);
 
-      const existingEventsCount = await IcalEvents.countDocuments(ioIcalMeta);
-
-      // This accounts for a situation where we ingest an empty or otherwise malformed iCal feed
-      if (existingEventsCount * 0.25 > events.length) {
-        console.log(
-          `Existing events count(${existingEventsCount}) is much greater than new events count(${events.length}) for icalUrlHash: ${icalUrlHash}, skipping`,
+        // This accounts for a situation where we ingest an empty or otherwise malformed iCal feed
+        if (existingEventsCount * 0.25 > events.length) {
+          console.log(
+            `Existing events count(${existingEventsCount}) is much greater than new events count(${events.length}) for icalUrlHash: ${icalUrlHash}, skipping`,
+          );
+          return;
+        }
+        const deleteResult = await IcalEvents.deleteMany(ioIcalMeta);
+        const insertResult = await IcalEvents.insertMany(
+          events.map((event) => ({
+            ...event,
+            recurrences: event.recurrences && Object.values(event.recurrences),
+            calendar,
+            _io_scrapedAt,
+            ...ioIcalMeta,
+          })),
         );
-        continue;
-      }
-      const deleteResult = await IcalEvents.deleteMany(ioIcalMeta);
-      const insertResult = await IcalEvents.insertMany(
-        events.map((event) => ({
-          ...event,
-          recurrences: event.recurrences && Object.values(event.recurrences),
-          calendar,
-          _io_scrapedAt,
-          ...ioIcalMeta,
-        })),
-      );
 
-      await Users.updateOne(
-        { _id: new ObjectId(user.id) },
-        {
-          $set: {
-            "dataSources.$[source].lastSuccessfulAt": new Date(),
-            "dataSources.$[source].lastSuccessfulRuntime": Date.now() - runtime,
-            "dataSources.$[source].lastResult": "success",
-          },
-        },
-        { arrayFilters: [{ "source.id": id }] },
-      );
-
-      yield {
-        icalUrlHash,
-        fetchedEventsCount: events.length,
-        existingEventsCount: existingEventsCount,
-        deletedCount: deleteResult.deletedCount,
-        insertedCount: insertResult.insertedCount,
-      };
+        yield {
+          icalUrlHash,
+          fetchedEventsCount: events.length,
+          existingEventsCount: existingEventsCount,
+          deletedCount: deleteResult.deletedCount,
+          insertedCount: insertResult.insertedCount,
+        };
+      });
     }
   });
