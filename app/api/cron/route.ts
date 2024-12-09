2e7409ef01490fb04cbf338ef0138bac96aec6cc
Io Klarstrup <io@klarstrup.dk>
2024-12-09 14:21:39 +0100

    fix(cron/route): exclude TopLogger data source due to issues

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,17 +1,17 @@
+import { compareAsc } from "date-fns";
 import { redirect } from "next/navigation";
 import { Users } from "../../../models/user.server";
 import { DataSource, UserDataSource } from "../../../sources/utils";
-import { compareAsc } from "date-fns";
 
 const sourceScrapeMap: Omit<
   Record<DataSource, string>,
-  DataSource.Fitocracy
+  DataSource.Fitocracy | DataSource.TopLogger
 > = {
   ical: "ical_scrape",
   myfitnesspal: "myfitnesspal_scrape",
   rundouble: "rundouble_scrape",
   tomorrow: "tomorrow_scrape",
-  toplogger: "toplogger_gql_scrape",
+  //  toplogger: "toplogger_gql_scrape",
   kilterboard: "kilterboard_scrape",
 };
 
@@ -21,7 +21,11 @@ export async function GET() {
   const dataSources = users
     .flatMap((user) => user.dataSources)
     .filter((dataSource): dataSource is UserDataSource => Boolean(dataSource))
-    .filter((dataSource) => dataSource.source !== DataSource.Fitocracy) // Fitocracy is read-only
+    .filter(
+      (dataSource) =>
+        dataSource.source !== DataSource.Fitocracy && // Fitocracy is read-only
+        dataSource.source !== DataSource.TopLogger, // TopLogger is acting up
+    )
     .sort((a, b) =>
       compareAsc(
         a.lastAttemptedAt ?? new Date(0),
