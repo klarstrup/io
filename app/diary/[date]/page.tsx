363304a56a167961c2f4a844eca7baf65913c7e5
Io Klarstrup <io@klarstrup.dk>
2025-03-09 22:50:27 +0100

    feat: integrate DiaryPoller with timezone handling in DiaryDayModal

--- app/diary/[date]/page.tsx
+++ app/diary/[date]/page.tsx
@@ -1,6 +1,10 @@
+import { TZDate } from "@date-fns/tz";
 import { auth } from "../../../auth";
 import { Modal } from "../../../components/Modal";
+import { DEFAULT_TIMEZONE } from "../../../utils";
+import { mostRecentlyScrapedAt } from "../actions";
 import { DiaryAgenda } from "../DiaryAgenda";
+import { DiaryPoller } from "../DiaryPoller";
 import { KeyHandler } from "./KeyHandler";
 
 export default async function DiaryDayModal(props: {
@@ -17,8 +21,16 @@ export default async function DiaryDayModal(props: {
     );
   }
 
+  const timeZone = user.timeZone || DEFAULT_TIMEZONE;
+  const now = TZDate.tz(timeZone);
+
   return (
     <Modal dismissTo={`/diary`}>
+      <DiaryPoller
+        mostRecentlyScrapedAtAction={mostRecentlyScrapedAt}
+        loadedAt={now}
+        userId={user.id}
+      />
       <KeyHandler date={date} />
       <div className="h-screen w-full max-w-3xl overflow-auto overscroll-contain rounded-xl bg-white p-2 shadow-xl shadow-black/50">
         <DiaryAgenda user={user} date={date} isModal />
