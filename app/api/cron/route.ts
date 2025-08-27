a81339bbc93074f2a87f3896afe2cc30a9d441fb
klarstrup <io@klarstrup.dk>
2025-08-27 19:22:34 +0200

    dont regularly scrape if not on vercel

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,6 +6,11 @@ import { DataSource, UserDataSource } from "../../../sources/utils";
 export async function GET() {
   const users = await Users.find({}).toArray();
 
+  if (!process.env.VERCEL) {
+    console.log("Skipping /cron scrape because we are not on Vercel");
+    return Response.json({});
+  }
+
   const dataSources = users
     .flatMap((user) => user.dataSources)
     .filter((dataSource): dataSource is UserDataSource => Boolean(dataSource))
