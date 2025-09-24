d51b003a3a9c653c18c220aa25d0d9edb97c745a
klarstrup <io@klarstrup.dk>
2025-09-24 21:01:01 +0200

    myfitnesspal_scrape: fix false-positive updates and improve orphan deletion

--- app/api/myfitnesspal_scrape/route.ts
+++ app/api/myfitnesspal_scrape/route.ts
@@ -7,7 +7,6 @@ import {
 } from "../../../sources/myfitnesspal.server";
 import { DataSource } from "../../../sources/utils";
 import { wrapSource } from "../../../sources/utils.server";
-import { isNonEmptyArray } from "../../../utils";
 import { jsonStreamResponse } from "../scraper-utils";
 
 const months = [
@@ -25,7 +24,9 @@ const months = [
   "12",
 ] as const;
 
-const years = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026] as const;
+const years = [
+  2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026,
+] as const;
 
 export const dynamic = "force-dynamic";
 export const maxDuration = 60;
@@ -69,42 +70,49 @@ export const GET = () =>
                 month,
               );
 
-              if (Array.isArray(reportEntries)) {
-                // Wipe the month to be replaced with the new data
-                await MyFitnessPalFoodEntries.deleteMany({
+              for (const reportEntry of reportEntries) {
+                for (const foodEntry of reportEntry.food_entries) {
+                  const { modifiedCount, upsertedCount } =
+                    await MyFitnessPalFoodEntries.updateOne(
+                      { id: foodEntry.id },
+                      {
+                        $set: {
+                          ...foodEntry,
+                          user_id: userId,
+                          datetime: DateTime.fromISO(foodEntry.date, {
+                            zone: "utc",
+                          }).toJSDate(),
+                        },
+                      },
+                      { upsert: true },
+                    );
+
+                  updatedDatabase ||= modifiedCount > 0 || upsertedCount > 0;
+                }
+
+                yield reportEntry.date;
+              }
+
+              const foodEntryIdsToBeDeleted = (
+                await MyFitnessPalFoodEntries.find({
                   user_id: userId,
                   date: { $regex: new RegExp(`^${year}-${month}-`) },
-                });
-              }
-              if (!isNonEmptyArray(reportEntries)) continue;
-              for (const reportEntry of reportEntries) {
-                if (reportEntry.food_entries) {
-                  for (const foodEntry of reportEntry.food_entries) {
-                    const updateResult =
-                      await MyFitnessPalFoodEntries.updateOne(
-                        { id: foodEntry.id },
-                        {
-                          $set: {
-                            ...foodEntry,
-                            user_id: userId,
-                            datetime: DateTime.fromISO(foodEntry.date, {
-                              zone: "utc",
-                            }).toJSDate(),
-                          },
-                        },
-                        { upsert: true },
-                      );
-                    if (
-                      updateResult.modifiedCount > 0 ||
-                      updateResult.upsertedCount > 0
-                    ) {
-                      updatedDatabase = true;
-                    }
-                  }
+                }).toArray()
+              )
+                .filter(
+                  (entry) =>
+                    !reportEntries
+                      .map((reportEntry) => reportEntry.food_entries)
+                      .flat()
+                      .some((foodEntry) => foodEntry.id === entry.id),
+                )
+                .map(({ id }) => id);
 
-                  yield reportEntry.date;
-                }
-              }
+              const { deletedCount } = await MyFitnessPalFoodEntries.deleteMany(
+                { id: { $in: foodEntryIdsToBeDeleted } },
+              );
+
+              if (deletedCount > 0) updatedDatabase ||= true;
             }
           }
 
