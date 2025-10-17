3f06e2d5b527d9359074eda9a12ffc8f4c89915c
klarstrup <io@klarstrup.dk>
2025-10-17 02:04:44 +0200

    wrapSources: inline wrapSource

--- sources/utils.server.ts
+++ sources/utils.server.ts
@@ -25,98 +25,89 @@ export async function* wrapSources<
     (ds): ds is DS => ds.source === source,
   );
   for (const dataSource of dataSources) {
-    yield* wrapSource(dataSource, user, (setUpdated) =>
-      fn(dataSource, setUpdated),
-    );
-  }
-}
-export async function* wrapSource<DS extends UserDataSource, T>(
-  dataSource: DS,
-  user: Session["user"],
-  fn: (setUpdated: SetUpdatedFn) => AsyncGenerator<T>,
-) {
-  const filter = { _id: new ObjectId(user.id) };
-  const updateOptions = { arrayFilters: [{ "source.id": dataSource.id }] };
+    const filter = { _id: new ObjectId(user.id) };
+    const updateOptions = { arrayFilters: [{ "source.id": dataSource.id }] };
 
-  const attemptedAt = new Date();
-  let updatedDatabase: boolean | null | undefined | void = null;
-  await Users.updateOne(
-    filter,
-    { $set: { "dataSources.$[source].lastAttemptedAt": attemptedAt } },
-    updateOptions,
-  );
-  try {
-    yield* fn((updated) => {
-      if (typeof updated !== "boolean") {
-        if ("modifiedCount" in updated) {
-          updatedDatabase ||=
-            updated.modifiedCount > 0 || updated.upsertedCount > 0;
+    const attemptedAt = new Date();
+    let updatedDatabase: boolean | null | undefined | void = null;
+    await Users.updateOne(
+      filter,
+      { $set: { "dataSources.$[source].lastAttemptedAt": attemptedAt } },
+      updateOptions,
+    );
+    try {
+      yield* fn(dataSource, (updated) => {
+        if (typeof updated !== "boolean") {
+          if ("modifiedCount" in updated) {
+            updatedDatabase ||=
+              updated.modifiedCount > 0 || updated.upsertedCount > 0;
+          } else {
+            updatedDatabase ||= updated.deletedCount > 0;
+          }
         } else {
-          updatedDatabase ||= updated.deletedCount > 0;
+          updatedDatabase ||= updated;
         }
-      } else {
-        updatedDatabase ||= updated;
-      }
 
-      return updatedDatabase;
-    });
+        return updatedDatabase;
+      });
 
-    const successfulAt = new Date();
-    await Users.updateOne(
-      filter,
-      {
-        $set: {
-          "dataSources.$[source].lastSuccessfulAt": successfulAt,
-          "dataSources.$[source].lastSuccessfulRuntime":
-            successfulAt.valueOf() - attemptedAt.valueOf(),
-          "dataSources.$[source].lastResult": "success",
+      const successfulAt = new Date();
+      await Users.updateOne(
+        filter,
+        {
+          $set: {
+            "dataSources.$[source].lastSuccessfulAt": successfulAt,
+            "dataSources.$[source].lastSuccessfulRuntime":
+              successfulAt.valueOf() - attemptedAt.valueOf(),
+            "dataSources.$[source].lastResult": "success",
+          },
         },
-      },
-      updateOptions,
-    );
-  } catch (e) {
-    const failedAt = new Date();
-    await Users.updateOne(
-      filter,
-      {
-        $set: {
-          "dataSources.$[source].lastFailedAt": failedAt,
-          "dataSources.$[source].lastFailedRuntime":
-            failedAt.valueOf() - attemptedAt.valueOf(),
-          "dataSources.$[source].lastError": String(e),
-          "dataSources.$[source].lastResult": "failure",
+        updateOptions,
+      );
+    } catch (e) {
+      const failedAt = new Date();
+      await Users.updateOne(
+        filter,
+        {
+          $set: {
+            "dataSources.$[source].lastFailedAt": failedAt,
+            "dataSources.$[source].lastFailedRuntime":
+              failedAt.valueOf() - attemptedAt.valueOf(),
+            "dataSources.$[source].lastError": String(e),
+            "dataSources.$[source].lastResult": "failure",
+          },
         },
-      },
-      updateOptions,
-    );
-    throw e;
-  } finally {
-    const materializer =
-      sourceToMaterializer[
-        dataSource.source as keyof typeof sourceToMaterializer
-      ];
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore - some runtimes think this is too complex
-    if (materializer) {
+        updateOptions,
+      );
+      throw e;
+    } finally {
+      const materializer =
+        sourceToMaterializer[
+          dataSource.source as keyof typeof sourceToMaterializer
+        ];
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore - some runtimes think this is too complex
-      yield* materializer?.(user, dataSource);
-    }
+      if (materializer) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore - some runtimes think this is too complex
+        yield* materializer?.(user, dataSource);
+      }
 
-    if (updatedDatabase !== false) {
-      try {
-        new PartySocket({
-          id: process.env.VERCEL_DEPLOYMENT_ID,
-          host: process.env.NEXT_PUBLIC_PARTYKIT_HOST ?? "localhost:1999",
-          room: user.id,
-        }).send(
-          JSON.stringify({
-            source: dataSource.source,
-            scrapedAt: new Date().valueOf(),
-          }),
-        );
-      } catch (error) {
-        console.error(error);
+      if (updatedDatabase !== false) {
+        try {
+          new PartySocket({
+            id: process.env.VERCEL_DEPLOYMENT_ID,
+            host: process.env.NEXT_PUBLIC_PARTYKIT_HOST ?? "localhost:1999",
+            room: user.id,
+          }).send(
+            JSON.stringify({
+              source: dataSource.source,
+              scrapedAt: new Date().valueOf(),
+            }),
+          );
+        } catch (error) {
+          console.error(error);
+        }
       }
     }
   }
