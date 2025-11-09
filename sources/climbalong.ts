8a6264bf6ea3425966fef1c287a4be8484aafd55
klarstrup <io@klarstrup.dk>
2025-11-09 01:57:32 +0100

    climbalong_scrape: support io being in multiple rounds

--- sources/climbalong.ts
+++ sources/climbalong.ts
@@ -1,4 +1,4 @@
-import { isAfter, isBefore, isFuture, max, min } from "date-fns";
+import { isAfter, isBefore } from "date-fns";
 import {
   type EventDetails,
   type EventEntry,
@@ -21,6 +21,7 @@ import {
   ClimbAlongNodes,
   ClimbAlongPerformances,
   ClimbAlongProblems,
+  ClimbAlongRounds,
 } from "./climbalong.server";
 
 export namespace Climbalong {
@@ -276,10 +277,11 @@ export async function getIoClimbAlongCompetitionEvent(
     ]),
   ]);
 
-  const [athletes, circuits, lanes] = await Promise.all([
+  const [athletes, circuits, lanes, rounds] = await Promise.all([
     ClimbAlongAthletes.find({ competitionId }).toArray(),
     ClimbAlongCircuits.find({ competitionId }).toArray(),
     ClimbAlongLanes.find({ competitionId }).toArray(),
+    ClimbAlongRounds.find({ competitionId }).toArray(),
   ]);
   const [performances, problems] = await Promise.all([
     ClimbAlongPerformances.find({
@@ -297,10 +299,10 @@ export async function getIoClimbAlongCompetitionEvent(
   const ioCircuitIds = ioPerformances
     ? unique(ioPerformances.map(({ circuitId }) => circuitId))
     : [];
-  const [ioCircuitChallengeNode, holds] = await Promise.all([
-    ClimbAlongNodes.findOne({
+  const [ioCircuitChallengeNodes, holds] = await Promise.all([
+    ClimbAlongNodes.find({
       "circuit.circuitId": { $in: ioCircuitIds },
-    }),
+    }).toArray(),
     ClimbAlongHolds.find({
       problemId: { $in: problems.map(({ problemId }) => problemId) },
     })
@@ -310,115 +312,6 @@ export async function getIoClimbAlongCompetitionEvent(
         holds.filter((hold) => hold.holdScore > 0),
       ),
   ]);
-  const circuitChallengeEdges = await ClimbAlongEdges.find({
-    processedBy: {
-      $elemMatch: {
-        nodeId: ioCircuitChallengeNode?.nodeId,
-        edge: ioCircuitChallengeNode?.outputEdgeIds[0],
-      },
-    },
-  }).toArray();
-
-  const noProblems = problems.filter(
-    (problem) =>
-      problem.circuitId === ioCircuitChallengeNode?.circuit.circuitId,
-  ).length;
-
-  let firstPerformance: Date | null = null;
-  let lastPerformance: Date | null = null;
-  for (const {
-    performanceStartedTime,
-    performanceEndedTime,
-    registrationTime,
-  } of ioPerformances || []) {
-    const start = new Date(performanceStartedTime);
-    if (!firstPerformance || isBefore(start, firstPerformance)) {
-      firstPerformance = start;
-    }
-
-    const end = new Date(performanceEndedTime || registrationTime);
-    if (!lastPerformance || isAfter(end, lastPerformance)) {
-      lastPerformance = end;
-    }
-  }
-
-  const ioCircuitChallenge =
-    io &&
-    circuitChallengeEdges.find(({ athletes }) =>
-      athletes.some((athlete) => athlete.athleteId === io?.athleteId),
-    );
-
-  const ioPerformanceSum = ioCircuitChallenge?.athletes.find(
-    (athlete) => athlete.athleteId === io?.athleteId,
-  )?.performanceSum;
-
-  // Using max rank for percentile calculations because some athletes DNF or not rank (i.e. GDPR deletion)
-  const maxRank = ioCircuitChallenge?.athletes.reduce(
-    (max, athlete) =>
-      athlete.performanceSum && athlete.performanceSum.rank > max
-        ? athlete.performanceSum.rank
-        : max,
-    0,
-  );
-
-  const noParticipants = ioCircuitChallenge?.athletes?.length || NaN;
-
-  const scores: Score[] = [];
-
-  if (
-    ioPerformanceSum &&
-    ioCircuitChallengeNode?.nodeOutputType.scoreSystem ===
-      Climbalong.ScoreSystem.BoulderTopAndZones
-  ) {
-    const descendingHolds = Array.from(holds).sort(
-      (a, b) => b.holdScore - a.holdScore,
-    );
-    const topScore = descendingHolds[0]?.holdScore;
-    const zoneScore = descendingHolds[descendingHolds.length - 1]?.holdScore;
-    const topSums = ioPerformanceSum.scoreSums.filter(
-      (sum) => sum.holdScore === topScore,
-    );
-    const zoneSums = ioPerformanceSum.scoreSums.filter(
-      (sum) => sum.holdScore === zoneScore,
-    );
-    scores.push({
-      system: SCORING_SYSTEM.TOPS_AND_ZONES,
-      source: SCORING_SOURCE.OFFICIAL,
-      rank: ioPerformanceSum.rank,
-      percentile: percentile(ioPerformanceSum.rank, maxRank ?? noParticipants),
-      tops: topSums.reduce(
-        (total, sum) => total + sum.totalNumberOfTimesReached,
-        0,
-      ),
-      zones: zoneSums.reduce(
-        (total, sum) => total + sum.totalNumberOfTimesReached,
-        0,
-      ),
-      topsAttempts: topSums.reduce(
-        (total, sum) => total + sum.totalNumberOfAttemptsUsed,
-        0,
-      ),
-      zonesAttempts: zoneSums.reduce(
-        (total, sum) => total + sum.totalNumberOfAttemptsUsed,
-        0,
-      ),
-    } satisfies TopsAndZonesScore);
-  } else if (
-    ioPerformanceSum &&
-    ioCircuitChallengeNode?.nodeOutputType.scoreSystem ===
-      Climbalong.ScoreSystem.BoulderOlympic
-  ) {
-    scores.push({
-      system: SCORING_SYSTEM.POINTS,
-      source: SCORING_SOURCE.OFFICIAL,
-      rank: ioPerformanceSum.rank,
-      percentile: percentile(ioPerformanceSum.rank, maxRank ?? noParticipants),
-      points:
-        typeof ioPerformanceSum.points === "number"
-          ? Math.round(ioPerformanceSum.points * 10) / 10
-          : NaN,
-    } satisfies PointsScore);
-  }
 
   let url = new URL(
     `https://climbalong.com/competitions/${competitionId}/info`,
@@ -427,17 +320,6 @@ export async function getIoClimbAlongCompetitionEvent(
     url = new URL(
       `https://climbalong.com/competitions/${competitionId}/results`,
     );
-
-    const ioLane =
-      ioCircuitChallengeNode &&
-      lanes.find((lane) => lane.endNodeId === ioCircuitChallengeNode.nodeId);
-
-    if (ioLane) {
-      url.searchParams.set("lane", String(ioLane.laneId));
-      // The comp link is not round-specific.
-      // EventDetails.rounds(defined below) may have more specific links in the future.
-      // url.searchParams.set("round", String(ioLane.roundId));
-    }
   }
 
   return {
@@ -448,86 +330,204 @@ export async function getIoClimbAlongCompetitionEvent(
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-non-null-asserted-optional-chain
     ioId: io?.athleteId!,
     url: url.toString(),
-    start: max(
-      [
-        firstPerformance && new Date(firstPerformance),
-        ioCircuitChallengeNode?.selfscoringOpen &&
-          new Date(ioCircuitChallengeNode?.selfscoringOpen),
-        new Date(competition.startTime),
-      ].filter(Boolean),
-    ),
-    end: isFuture(new Date(competition.endTime))
-      ? new Date(competition.endTime)
-      : min(
-          [
-            lastPerformance && new Date(lastPerformance),
-            ioCircuitChallengeNode?.selfscoringClose &&
-              new Date(ioCircuitChallengeNode?.selfscoringClose),
-            new Date(competition.endTime),
-          ].filter(Boolean),
-        ),
-    venue: competition.facility.trim(),
-    eventName: competition.title.trim(),
+    start: new Date(competition.startTime),
+    end: new Date(competition.endTime),
+    venue: competition.facility,
+    eventName: competition.title,
     location: competition.address,
-    rounds: [
-      {
-        id:
-          ioCircuitChallengeNode?.circuit.circuitId ||
-          competition.competitionId,
-        category:
-          ioCircuitChallenge?.title
-            ?.replace("Mænd/", "")
-            ?.replace("Male / ", "")
-            ?.replace("Male", "Open") || null,
-        noParticipants,
-        problems: noProblems,
-        problemByProblem: problems.length
-          ? Array.from(
-              problems
-                .filter((problem) => ioCircuitIds.includes(problem.circuitId))
-                .reduce((memo, problem) => {
-                  const ioPerformance = ioPerformances?.find(
-                    (performance) =>
-                      performance.problemId === problem.problemId,
-                  );
+    rounds: await Promise.all(
+      ioCircuitChallengeNodes.map(async (ioCircuitChallengeNode) => {
+        const circuitChallengeEdges = await ClimbAlongEdges.find({
+          processedBy: {
+            $elemMatch: {
+              nodeId: ioCircuitChallengeNode.nodeId,
+              edge: ioCircuitChallengeNode.outputEdgeIds[0],
+            },
+          },
+        }).toArray();
 
-                  // More nastiness here because each problem is repeated for each lane
-                  const key = problem.title;
-                  memo.set(key, {
-                    number: problem.title,
-                    color: undefined,
-                    grade: undefined,
-                    attempt: Boolean(
-                      ioPerformance?.numberOfAttempts || memo.get(key)?.attempt,
-                    ),
-                    attemptCount: ioPerformance?.numberOfAttempts,
-                    zone: Boolean(
-                      ioPerformance?.scores.some((score) =>
-                        isZone(score, holds),
-                      ) || memo.get(key)?.zone,
-                    ),
-                    top: Boolean(
-                      ioPerformance?.scores.some((score) =>
-                        isTop(score, holds),
-                      ) || memo.get(key)?.top,
-                    ),
-                    flash: Boolean(
-                      ioPerformance?.scores.some(
-                        (score) =>
-                          isTop(score, holds) && score.reachedInAttempt === 1,
-                      ) || memo.get(key)?.flash,
-                    ),
-                    repeat: false,
-                  });
+        const noProblems = problems.filter(
+          (problem) =>
+            ioCircuitChallengeNode.circuit.circuitId === problem.circuitId,
+        ).length;
 
-                  return memo;
-                }, new Map<string, PP>())
-                .values(),
-            )
-          : null,
-        scores,
-      },
-    ],
+        let firstPerformance: Date | null = null;
+        let lastPerformance: Date | null = null;
+        for (const {
+          performanceStartedTime,
+          performanceEndedTime,
+          registrationTime,
+        } of ioPerformances || []) {
+          const start = new Date(performanceStartedTime);
+          if (!firstPerformance || isBefore(start, firstPerformance)) {
+            firstPerformance = start;
+          }
+
+          const end = new Date(performanceEndedTime || registrationTime);
+          if (!lastPerformance || isAfter(end, lastPerformance)) {
+            lastPerformance = end;
+          }
+        }
+
+        const circuitChallengeAthletes =
+          io &&
+          circuitChallengeEdges.find(({ athletes }) =>
+            athletes.some((athlete) => athlete.athleteId === io.athleteId),
+          )?.athletes;
+
+        const ioPerformanceSum = circuitChallengeAthletes?.find(
+          (athlete) => athlete.athleteId === io?.athleteId,
+        )?.performanceSum;
+
+        // Using max rank for percentile calculations because some athletes DNF or not rank (i.e. GDPR deletion)
+        const maxRank = circuitChallengeAthletes?.reduce(
+          (max, athlete) =>
+            athlete.performanceSum && athlete.performanceSum.rank > max
+              ? athlete.performanceSum.rank
+              : max,
+          0,
+        );
+
+        const noParticipants = circuitChallengeAthletes?.length || NaN;
+
+        const scores: Score[] = [];
+
+        if (
+          ioPerformanceSum &&
+          ioCircuitChallengeNode.nodeOutputType.scoreSystem ===
+            Climbalong.ScoreSystem.BoulderTopAndZones
+        ) {
+          const descendingHolds = Array.from(holds).sort(
+            (a, b) => b.holdScore - a.holdScore,
+          );
+          const topScore = descendingHolds[0]?.holdScore;
+          const zoneScore =
+            descendingHolds[descendingHolds.length - 1]?.holdScore;
+          const topSums = ioPerformanceSum.scoreSums.filter(
+            (sum) => sum.holdScore === topScore,
+          );
+          const zoneSums = ioPerformanceSum.scoreSums.filter(
+            (sum) => sum.holdScore === zoneScore,
+          );
+          scores.push({
+            system: SCORING_SYSTEM.TOPS_AND_ZONES,
+            source: SCORING_SOURCE.OFFICIAL,
+            rank: ioPerformanceSum.rank,
+            percentile: percentile(
+              ioPerformanceSum.rank,
+              maxRank ?? noParticipants,
+            ),
+            tops: topSums.reduce(
+              (total, sum) => total + sum.totalNumberOfTimesReached,
+              0,
+            ),
+            zones: zoneSums.reduce(
+              (total, sum) => total + sum.totalNumberOfTimesReached,
+              0,
+            ),
+            topsAttempts: topSums.reduce(
+              (total, sum) => total + sum.totalNumberOfAttemptsUsed,
+              0,
+            ),
+            zonesAttempts: zoneSums.reduce(
+              (total, sum) => total + sum.totalNumberOfAttemptsUsed,
+              0,
+            ),
+          } satisfies TopsAndZonesScore);
+        } else if (
+          ioPerformanceSum &&
+          ioCircuitChallengeNode.nodeOutputType.scoreSystem ===
+            Climbalong.ScoreSystem.BoulderOlympic
+        ) {
+          scores.push({
+            system: SCORING_SYSTEM.POINTS,
+            source: SCORING_SOURCE.OFFICIAL,
+            rank: ioPerformanceSum.rank,
+            percentile: percentile(
+              ioPerformanceSum.rank,
+              maxRank ?? noParticipants,
+            ),
+            points:
+              typeof ioPerformanceSum.points === "number"
+                ? Math.round(ioPerformanceSum.points * 10) / 10
+                : NaN,
+          } satisfies PointsScore);
+        }
+
+        const lane = lanes.find(({ startNodeId, startEdgeId }) =>
+          ioCircuitChallengeNode.inputsFrom.some(
+            ({ nodeId, edgeId }) =>
+              nodeId === startNodeId && edgeId === startEdgeId,
+          ),
+        );
+        const round =
+          lane && rounds.find((round) => round.roundId === lane.roundId);
+
+        return {
+          id:
+            ioCircuitChallengeNode.circuit.circuitId ||
+            competition.competitionId,
+          roundName: round?.title,
+          category:
+            lane?.title
+              ?.replace("Mænd/", "")
+              ?.replace("Male / ", "")
+              ?.replace("Male", "Open") || null,
+          noParticipants,
+          problems: noProblems,
+          problemByProblem: problems.length
+            ? Array.from(
+                problems
+                  .filter(
+                    (problem) =>
+                      ioCircuitChallengeNode.circuit.circuitId ===
+                      problem.circuitId,
+                  )
+                  .reduce((memo, problem) => {
+                    const ioPerformance = ioPerformances?.find(
+                      (performance) =>
+                        performance.problemId === problem.problemId,
+                    );
+
+                    // More nastiness here because each problem is repeated for each lane
+                    const key = problem.title;
+                    memo.set(key, {
+                      number: problem.title,
+                      color: undefined,
+                      grade: undefined,
+                      attempt: Boolean(
+                        ioPerformance?.numberOfAttempts ||
+                          memo.get(key)?.attempt,
+                      ),
+                      attemptCount: ioPerformance?.numberOfAttempts,
+                      zone: Boolean(
+                        ioPerformance?.scores.some((score) =>
+                          isZone(score, holds),
+                        ) || memo.get(key)?.zone,
+                      ),
+                      top: Boolean(
+                        ioPerformance?.scores.some((score) =>
+                          isTop(score, holds),
+                        ) || memo.get(key)?.top,
+                      ),
+                      flash: Boolean(
+                        ioPerformance?.scores.some(
+                          (score) =>
+                            isTop(score, holds) && score.reachedInAttempt === 1,
+                        ) || memo.get(key)?.flash,
+                      ),
+                      repeat: false,
+                    });
+
+                    return memo;
+                  }, new Map<string, PP>())
+                  .values(),
+              )
+            : null,
+          scores,
+        };
+      }),
+    ),
   } as const;
 }
 
@@ -546,8 +546,8 @@ export async function getIoClimbAlongCompetitionEventEntry(
     type: "competition",
     discipline: "bouldering",
     id: competitionId,
-    venue: competition.facility.trim(),
-    eventName: competition.title.trim(),
+    venue: competition.facility,
+    eventName: competition.title,
     location: competition.address,
     ioId: athleteId,
     start: new Date(competition.startTime),
