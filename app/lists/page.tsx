d9e63fe0019a450fc08f575c433552fcd7581920
klarstrup <io@klarstrup.dk>
2026-01-01 17:13:59 +0100

    Implement ListPage to fetch and display user calendar todos

--- app/lists/page.tsx
+++ app/lists/page.tsx
@@ -1,3 +1,37 @@
-export default function ListPage() {
-  return <div className="bg-green-50">List Page</div>;
+import { auth } from "../../auth";
+import { getUserIcalTodosBetween } from "../../sources/ical";
+import { DiaryAgendaDayTodo } from "../diary/DiaryAgendaDayTodo";
+
+export default async function ListPage() {
+  const user = (await auth())?.user;
+
+  const calendarTodos = user ? await getUserIcalTodosBetween(user.id) : [];
+  const todos = calendarTodos.filter((todo) => !todo.completed);
+  const todones = calendarTodos.filter((todo) => todo.completed);
+
+  return (
+    <div className="max-h-screen min-h-screen">
+      <div className="mx-auto max-h-screen max-w-2xl self-stretch border-black/25 px-2">
+        <div
+          className="grid gap-1.5 pb-1.5"
+          style={{ gridTemplateColumns: "1.25rem minmax(0, 1fr)" }}
+        >
+          {todos.map((todo) => (
+            <DiaryAgendaDayTodo todo={todo} key={todo.uid} />
+          ))}
+          {todos.length && todones.length ? (
+            <>
+              <span></span>
+              <span>
+                <hr className="border-gray-200" />
+              </span>
+            </>
+          ) : null}
+          {todones.map((todo) => (
+            <DiaryAgendaDayTodo todo={todo} key={todo.uid} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
