2fd2fda5600b5b056b252d0351339fea62dcfe09
klarstrup <io@klarstrup.dk>
2026-01-25 00:10:12 +0100

    /diary: add loading page

---
+++ app/diary/loading.tsx
@@ -0,0 +1,12 @@
+import { FieldSetY } from "../../components/FieldSet";
+
+export default function Loading() {
+  return (
+    <FieldSetY
+      legend={null}
+      className="mx-auto max-w-2xl self-stretch border-black/50 bg-gray-500/25 px-2"
+    >
+      <center className="text-white">Loading journal...</center>
+    </FieldSetY>
+  );
+}
