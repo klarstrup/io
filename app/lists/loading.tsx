50790c6c9db96cc7a97f543ba6c247d472c53db9
klarstrup <io@klarstrup.dk>
2026-01-24 22:30:18 +0100

    /lists: improve top level loading page

--- app/lists/loading.tsx
+++ app/lists/loading.tsx
@@ -1,3 +1,12 @@
+import { FieldSetY } from "../../components/FieldSet";
+
 export default function Loading() {
-  return <center>Loading todos...</center>;
+  return (
+    <FieldSetY
+      legend={null}
+      className="mx-auto max-w-2xl self-stretch border-black/50 bg-gray-500/25 px-2"
+    >
+      <center className="text-white">Loading todos...</center>
+    </FieldSetY>
+  );
 }
