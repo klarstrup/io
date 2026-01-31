86b21171fc3efdd6466083d17ee69feaa039f54e
klarstrup <io@klarstrup.dk>
2026-01-31 15:01:50 +0100

    fix(ApolloFucker): give post-restoration refetching a second

--- ApolloFucker.tsx
+++ ApolloFucker.tsx
@@ -3,21 +3,21 @@ import { useEffect } from "react";
 
 export default function ApolloFucker() {
   const client = useApolloClient();
-
   useEffect(() => {
     const cache = client.cache;
     // Refetch things after restoring from persisted cache
     if (typeof window !== "undefined") {
-      // If there's anything in the cache this soon as the client is created,
-      console.log(cache);
-      // @ts-expect-error - failed to monkey patch this in the types
-      if (cache._io_wasRestoredFromLocalStorage) {
-        requestIdleCallback(() => {
-          client.refetchObservableQueries().catch(() => {
-            // don't care
+      setTimeout(() => {
+        // If there's anything in the cache this soon as the client is created,
+        // @ts-expect-error - failed to monkey patch this in the types
+        if (cache._io_wasRestoredFromLocalStorage) {
+          requestIdleCallback(() => {
+            client.refetchObservableQueries().catch(() => {
+              // don't care
+            });
           });
-        });
-      }
+        }
+      }, 1000);
     }
   }, [client]);
   return null;
