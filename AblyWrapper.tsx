daf9b6d38528136a0cf9f43b9b6fe0843edb0345
klarstrup <io@klarstrup.dk>
2026-02-01 01:22:34 +0100

    fix(AblyWrapper): ensure realtime client is only created in the browser

--- AblyWrapper.tsx
+++ AblyWrapper.tsx
@@ -3,18 +3,19 @@ import * as Ably from "ably";
 import { AblyProvider, ChannelProvider } from "ably/react";
 
 // Create your Ably Realtime client
-const realtimeClient = new Ably.Realtime({
-  key: "qb4STQ.ARokgg:e0n62NscHz8TTXG9PaGb26OWks0IBp-LSKIxh7LmxFw",
-});
+const realtimeClient =
+  typeof window === "undefined"
+    ? null
+    : new Ably.Realtime({
+        key: "qb4STQ.ARokgg:e0n62NscHz8TTXG9PaGb26OWks0IBp-LSKIxh7LmxFw",
+      });
 
 export default function AblyWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  if (typeof window === "undefined") {
-    return <>{children}</>;
-  }
+  if (!realtimeClient) return <>{children}</>;
 
   return (
     <AblyProvider client={realtimeClient}>
