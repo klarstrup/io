d872fd372680d0428c1f9aac797fabb19be22f74
klarstrup <io@klarstrup.dk>
2026-02-02 18:39:50 +0100

    refactor(route): remove generateStaticParams export from createSerwistRoute
    it breaks the build idk

--- app/serwist/[path]/route.ts
+++ app/serwist/[path]/route.ts
@@ -15,14 +15,13 @@ const revision =
   spawnSync("git", ["rev-parse", "HEAD"], { encoding: "utf-8" }).stdout ??
   crypto.randomUUID();
 
-export const { dynamic, dynamicParams, revalidate, generateStaticParams, GET } =
-  createSerwistRoute({
-    additionalPrecacheEntries: [
-      { url: "/~offline", revision },
-      { url: "/~shell", revision },
-    ],
-    swSrc: "app/sw.ts",
-    nextConfig,
-    // If set to `false`, Serwist will attempt to use `esbuild-wasm`.
-    useNativeEsbuild: true,
-  });
+export const { dynamic, dynamicParams, revalidate, GET } = createSerwistRoute({
+  additionalPrecacheEntries: [
+    { url: "/~offline", revision },
+    { url: "/~shell", revision },
+  ],
+  swSrc: "app/sw.ts",
+  nextConfig,
+  // If set to `false`, Serwist will attempt to use `esbuild-wasm`.
+  useNativeEsbuild: true,
+});
