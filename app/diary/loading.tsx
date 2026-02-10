1b036ca3f7a6b03f3447b8c792438f481d974e0f
klarstrup <io@klarstrup.dk>
2026-02-10 15:22:58 +0100

    fix: update layout structure in Loading and DiaryLayout components for improved styling

--- app/diary/loading.tsx
+++ app/diary/loading.tsx
@@ -2,11 +2,11 @@ import { FieldSetY } from "../../components/FieldSet";
 
 export default function Loading() {
   return (
-    <div className="max-h-screen min-h-screen">
-      <div className="mx-auto max-h-screen max-w-2xl self-stretch border-black/25 px-2">
+    <div className="flex min-h-screen flex-col justify-center py-2">
+      <div className="mx-auto max-h-screen max-w-2xl self-stretch px-2">
         <FieldSetY
           legend={null}
-          className="mx-auto max-w-2xl self-stretch border-black/50 bg-gray-500/25 px-2"
+          className="mx-auto max-w-2xl border-black/50 bg-gray-500/25 px-2"
         >
           <center className="text-white">Loading journal...</center>
         </FieldSetY>
--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -8,8 +8,8 @@ export default async function DiaryLayout(_props: PageProps<"/diary">) {
   const user = (await auth())?.user;
 
   return (
-    <div className="max-h-screen min-h-screen py-2">
-      <div className="mx-auto max-h-screen max-w-2xl self-stretch border-black/25 px-2">
+    <div className="flex min-h-screen flex-col justify-center py-2">
+      <div className="mx-auto max-h-screen max-w-2xl self-stretch px-2">
         <TodoDragDropContainer userId={user?.id}>
           <DiaryAgendaDay user={user} />
         </TodoDragDropContainer>
