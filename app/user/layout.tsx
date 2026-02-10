15b8df33f9ecfbfa45ea4b0364e9f020f55cb0a3
io <io@klarstrup.dk>
2026-02-10 12:22:40 +0100

    Fix layout class names for UserLayout component

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,7 +1,7 @@
 export default function UserLayout({ children }: LayoutProps<"/user">) {
   return (
-    <div className="max-h-screen min-h-screen">
-      <div className="mx-auto max-h-screen max-w-xl self-stretch rounded-xl border border-black/25 bg-white p-2">
+    <div className="min-H-screen p-2">
+      <div className="mx-auto max-w-xl self-stretch rounded-xl border border-black/25 bg-white p-2">
         {children}
       </div>
     </div>
