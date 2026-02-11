491ca70e4a350571f46ef035c410a59678715b4c
klarstrup <io@klarstrup.dk>
2026-02-11 10:39:37 +0100

    LoadingIndicator: Keep spinning when not showing

--- app/LoadingIndicator.tsx
+++ app/LoadingIndicator.tsx
@@ -4,15 +4,19 @@ import { useApolloNetworkStatus } from "../ApolloWrapper";
 
 export default function LoadingIndicator() {
   const networkStatus = useApolloNetworkStatus();
-  if (
-    networkStatus.numPendingQueries === 0 &&
-    networkStatus.numPendingMutations === 0
-  ) {
-    return null;
-  }
+
+  // Don't return early because I want the div to be animating continuously,
+  // so it doesn't jump back to 0 rotation when the network status changes.
+  // Instead, conditionally render the hourglass inside the div.
+
   return (
-    <div className="fixed z-50 top-4 right-4 flex items-center justify-center p-4">
-      <span className="animate-spin text-4xl">⏳</span>
+    <div className="pointer-events-none fixed top-4 right-4 z-50 flex items-center justify-center p-4">
+      <span className="animate-spin text-4xl">
+        {networkStatus.numPendingQueries === 0 &&
+        networkStatus.numPendingMutations === 0
+          ? null
+          : "⏳"}
+      </span>
     </div>
   );
 }
