02fbb1f1714c1570ac7e23df31e63177bee4e62c
klarstrup <io@klarstrup.dk>
2026-02-17 00:21:43 +0100

    DiaryAgendaDayWorkoutSet: fix non-self source style

--- app/diary/DiaryAgendaDayWorkoutSet.tsx
+++ app/diary/DiaryAgendaDayWorkoutSet.tsx
@@ -150,9 +150,11 @@ export function DiaryAgendaDayWorkoutSet({
         {setsWithLocation.length > 0 ? (
           <div
             className={
-              "border-l-black/20 flex flex-1 items-center border-l text-xs " +
+              "flex flex-1 items-center text-xs " +
               (isClimbingExercise(exerciseInfo.id) ? " pb-1" : " ") +
-              (workout.source === WorkoutSource.Self ? " px-1 py-0.5" : "")
+              (workout.source === WorkoutSource.Self
+                ? " border-l border-l-black/20 px-1 py-0.5"
+                : "")
             }
           >
             <WorkoutEntryExercise
