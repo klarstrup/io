89c3418b50b8a09d2d96213dd53ddc2bab0064f5
klarstrup <io@klarstrup.dk>
2026-02-17 00:16:02 +0100

    feat(graphql): add Apollo Server usage reporting plugin

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -3,8 +3,15 @@ import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { NextRequest } from "next/server";
 import { resolvers, typeDefs } from "../../../graphql";
 import { auth } from "../../../auth";
+import { ApolloServerPluginUsageReporting } from "@apollo/server/plugin/usageReporting";
 
-const server = new ApolloServer({ resolvers, typeDefs });
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+  plugins: [
+    ApolloServerPluginUsageReporting({ sendVariableValues: { all: true } }),
+  ],
+});
 
 const handler = startServerAndCreateNextHandler(server, {
   context: async () => ({ user: (await auth())?.user || null }),
