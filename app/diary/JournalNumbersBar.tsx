1f940f4e8df2f5d782723535147aebd1963eec15
klarstrup <io@klarstrup.dk>
2026-02-17 15:25:02 +0100

    fix(JournalNumbersBar): update GraphQL query import and refine query structure

--- app/diary/JournalNumbersBar.tsx
+++ app/diary/JournalNumbersBar.tsx
@@ -2,20 +2,22 @@
 
 import { useQuery } from "@apollo/client/react";
 import gql from "graphql-tag";
-import { GetLatestWeightEntryQuery } from "../../graphql.generated";
+import { GetLatestWeightEntryDocument } from "../../graphql.generated";
+
+gql`
+  query GetLatestWeightEntry {
+    user {
+      weight
+    }
+  }
+`;
 
 export default function JournalNumbersBar() {
-  const { data } = useQuery<GetLatestWeightEntryQuery>(gql`
-    query GetLatestWeightEntry {
-      user {
-        weight
-      }
-    }
-  `);
+  const { data } = useQuery(GetLatestWeightEntryDocument);
 
   return (
     <div
-      className="fixed left-1/2 z-50 flex -translate-x-1/2 transform items-center gap-1 rounded-2xl border border-[yellow]/25 bg-white/10 px-2 py-1 backdrop-blur-md sm:gap-2 pointer-coarse:top-2 pointer-fine:bottom-4"
+      className="fixed left-1/2 z-50 flex -translate-x-1/2 transform items-center gap-1 border border-[yellow]/25 bg-white/10 px-2 py-1 backdrop-blur-md sm:gap-2 pointer-coarse:top-0 pointer-coarse:rounded-b-2xl pointer-fine:bottom-0 pointer-fine:rounded-t-2xl"
       style={{
         boxShadow:
           "0 0 48px rgba(0, 0, 0, 0.5), 0 0 24px #edab00, 0 0 24px #edab00, 0 0 6px rgba(0, 0, 0, 1), 0 0 1px rgba(0, 0, 0, 1)",
