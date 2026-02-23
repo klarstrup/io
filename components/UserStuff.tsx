391af0148e0edbfb8c6c17db94685352de6f6e9d
klarstrup <io@klarstrup.dk>
2026-02-23 09:56:47 +0100

    fix: adjust max-width calculations in DashBar and UserStuff components for improved layout

--- components/UserStuff.tsx
+++ components/UserStuff.tsx
@@ -7,7 +7,7 @@ import UserStuffLink from "./UserStuffLink";
 export default function UserStuff() {
   return (
     <div
-      className="fixed left-1/2 z-50 flex max-w-[calc(100%-2.5rem)] -translate-x-1/2 transform items-center gap-x-2 rounded-2xl border border-[yellow]/25 bg-white/10 py-0.5 pr-0 overflow-hidden pl-2 backdrop-blur-md sm:gap-2 pointer-coarse:bottom-2 pointer-fine:top-4"
+      className="fixed left-1/2 z-50 flex max-w-[calc(100%-0.5rem)] -translate-x-1/2 transform items-center gap-x-2 rounded-2xl border border-[yellow]/25 bg-white/10 py-0.5 pr-0 overflow-hidden pl-2 backdrop-blur-md sm:gap-2 pointer-coarse:bottom-2 pointer-fine:top-4"
       style={{
         boxShadow:
           "0 0 48px rgba(0, 0, 0, 0.5), 0 0 24px #edab00, 0 0 24px #edab00, 0 0 6px rgba(0, 0, 0, 1), 0 0 1px rgba(0, 0, 0, 1)",
