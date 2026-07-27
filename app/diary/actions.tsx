f44d887d6c82b6c0b580ad60d620088a78f17276
klarstrup <io@klarstrup.dk>
2026-07-27 15:53:05 +0200

    refactor(actions): remove upsertWorkout function and clean up imports

--- app/diary/actions.tsx
+++ app/diary/actions.tsx
@@ -6,11 +6,10 @@ import { revalidatePath } from "next/cache";
 import PartySocket from "partysocket";
 import { v4 as uuid } from "uuid";
 import { auth } from "../../auth";
-import { LocationData } from "../../models/location";
+import type { LocationData } from "../../models/location";
 import { Locations } from "../../models/location.server";
 import type { ITodoScheduleWithExerciseProgram } from "../../models/user";
 import { Users } from "../../models/user.server";
-import { type WorkoutData } from "../../models/workout";
 import {
   updateExerciseCounts,
   updateLocationCounts,
@@ -36,69 +35,6 @@ const emitIoUpdate = (userId: string) => {
     console.error(error);
   }
 };
-
-export async function upsertWorkout(
-  workout:
-    | (Omit<WorkoutData, "id"> & { _id: string })
-    | Omit<WorkoutData, "id">,
-) {
-  const user = (await auth())?.user;
-  if (!user || workout.userId !== user.id) throw new Error("Unauthorized");
-  console.time("upsertWorkout");
-
-  console.time("upsertWorkout:upsertWorkout");
-
-  // Check if locationId is provided and if no such location exists with that
-  // locationId as its name or its _id, create it with that as the name and
-  // use it as the locationId in the workout document
-  // eslint-disable-next-line @typescript-eslint/no-deprecated
-  if (workout.locationId && !workout.location) {
-    const newLocationName = workout.locationId.trim();
-    const location = await Locations.findOne(
-      ObjectId.isValid(workout.locationId)
-        ? { _id: new ObjectId(workout.locationId) }
-        : { name: newLocationName },
-    );
-    if (!location) {
-      const now = new Date();
-      const newLocation = await Locations.insertOne({
-        name: workout.locationId,
-        userId: user.id,
-        createdAt: now,
-        updatedAt: now,
-      });
-      workout.locationId = newLocation.insertedId.toString();
-    } else {
-      workout.locationId = location._id.toString();
-    }
-  }
-
-  let _id: ObjectId;
-  if ("_id" in workout) {
-    const { _id: id, ...rest } = workout;
-    _id = new ObjectId(id);
-    await Workouts.updateOne({ _id }, { $set: rest });
-  } else {
-    const newWorkout = await Workouts.insertOne(workout);
-    _id = newWorkout.insertedId;
-  }
-  console.timeEnd("upsertWorkout:upsertWorkout");
-
-  waitUntil(updateLocationCounts(user.id));
-  waitUntil(updateExerciseCounts(user.id));
-
-  console.time("upsertWorkout:materializeAllIoWorkouts");
-  await arrayFromAsyncIterable(materializeIoWorkouts(user));
-  console.timeEnd("upsertWorkout:materializeAllIoWorkouts");
-
-  revalidatePath("/diary");
-
-  console.timeEnd("upsertWorkout");
-
-  emitIoUpdate(user.id);
-
-  return String(_id);
-}
 
 export async function deleteWorkout(workoutId: string) {
   const user = (await auth())?.user;
