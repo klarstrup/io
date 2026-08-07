8a129858dc023f28dea3c25dcf1ee26026c2d45d
klarstrup <io@klarstrup.dk>
2026-08-07 10:20:58 +0200

    fix(layout): move Backdrop component outside of Suspense
    it shant suspend

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,8 +34,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
               <Suspense>
                 <UserStuff />
                 {children}
-                <Backdrop />
               </Suspense>
+              <Backdrop />
               <Analytics />
             </AblyWrapper>
           </ApolloWrapper>
