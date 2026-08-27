867341653b5e6e1cf94570e23b8cae81d13a548a
klarstrup <io@klarstrup.dk>
2026-08-27 09:47:06 +0200

    style: update layout for Event Entry in JournalEntryModal

--- app/diary/JournalEntryModal.tsx
+++ app/diary/JournalEntryModal.tsx
@@ -28,7 +28,12 @@ export default function JournalEntryModal({ entryId }: { entryId: string }) {
       <div>
         <h1>Event Entry</h1>
         <p className="break-all">Entity ID: {entityId}</p>
-        <div className="mt-4 flex max-w-full flex-wrap gap-2">
+        <div
+          className="mt-4 grid max-w-full gap-2 border-t border-black/10 pt-4"
+          style={{
+            gridTemplateColumns: "repeat(auto-fill, minmax(160px, 1fr))",
+          }}
+        >
           <SourceWidget dataSource={DataSource.ICal} />
         </div>
       </div>
