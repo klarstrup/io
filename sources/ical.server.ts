9cc206c325144a3719a131c00bcf3e51296c1266
klarstrup <io@klarstrup.dk>
2026-09-06 21:30:40 +0200

    fix: simplify filter logic for user Ical todos retrieval

--- sources/ical.server.ts
+++ sources/ical.server.ts
@@ -232,12 +232,7 @@ export async function* getUserIcalTodosBetween(
             { completed: { $gte: start, $lte: end } },
           ],
         }
-      : ({
-          $and: [
-            { $or: [{ due: { $exists: false } }, { due: null }] },
-            { $or: [{ completed: { $exists: false } }, { completed: null }] },
-          ],
-        } satisfies FilterOperators<Omit<VTodo, "recurrences">>);
+      : {};
 
   for await (const todo of (IcalEvents as ProxyCollection<MongoVTodo>).find(
     {
